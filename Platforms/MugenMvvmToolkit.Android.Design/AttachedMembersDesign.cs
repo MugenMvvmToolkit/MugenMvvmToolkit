cd174a8289d641d8de6fc48dfb85b0547fbf50c8
Vyacheslav Volkov <vvs0205@outlook.com>
2015-11-04 11:50:38 +0700

    Added the license headers.

--- Platforms/MugenMvvmToolkit.Android.Design/AttachedMembersDesign.cs
+++ Platforms/MugenMvvmToolkit.Android.Design/AttachedMembersDesign.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿#region Copyright
+
+// ****************************************************************************
+// <copyright file="AttachedMembersDesign.cs">
+// Copyright (c) 2012-2015 Vyacheslav Volkov
+// </copyright>
+// ****************************************************************************
+// <author>Vyacheslav Volkov</author>
+// <email>vvs0205@outlook.com</email>
+// <project>MugenMvvmToolkit</project>
+// <web>https://github.com/MugenMvvmToolkit/MugenMvvmToolkit</web>
+// <license>
+// See license.txt in this solution or http://opensource.org/licenses/MS-PL
+// </license>
+// ****************************************************************************
+
+#endregion
+
+using System;
 using Android.Views;
 using MugenMvvmToolkit.Android.Binding;
 using MugenMvvmToolkit.Binding;
