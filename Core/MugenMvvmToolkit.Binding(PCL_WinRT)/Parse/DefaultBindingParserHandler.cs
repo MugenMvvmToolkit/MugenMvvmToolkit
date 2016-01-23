475cc723225d1f3c20c5bb53f8f358730aec7460
Vyacheslav Volkov <vvs0205@outlook.com>
2016-01-23 16:30:37 +0700

    Fixed incorrect interpolated strings check

--- Core/MugenMvvmToolkit.Binding(PCL_WinRT)/Parse/DefaultBindingParserHandler.cs
+++ Core/MugenMvvmToolkit.Binding(PCL_WinRT)/Parse/DefaultBindingParserHandler.cs
@@ -83,7 +83,7 @@ namespace MugenMvvmToolkit.Binding.Parse
             foreach (var replaceKeyword in ReplaceKeywords)
                 bindingExpression = bindingExpression.Replace(replaceKeyword.Key, replaceKeyword.Value);
 
-            if (!bindingExpression.Contains("$\"{") && !bindingExpression.Contains("$'{"))
+            if (!bindingExpression.Contains("$\"") && !bindingExpression.Contains("$'"))
                 return;
             Dictionary<string, string> dict = null;
             InterpolatedStringTokenizer.SetSource(bindingExpression);
