46aeafbb61323f0a6d2ab6d1fcce147e99c90541
Vyacheslav Volkov <vvs0205@outlook.com>
2016-04-24 01:43:38 +0700

    added the BeginUpdates\EndUpdates method calls

--- Platforms/MugenMvvmToolkit.iOS/Binding/Infrastructure/ItemsSourceTableViewSource.cs
+++ Platforms/MugenMvvmToolkit.iOS/Binding/Infrastructure/ItemsSourceTableViewSource.cs
@@ -152,11 +152,17 @@ namespace MugenMvvmToolkit.iOS.Binding.Infrastructure
             {
                 case NotifyCollectionChangedAction.Add:
                     NSIndexPath[] newIndexPaths = PlatformExtensions.CreateNSIndexPathArray(args.NewStartingIndex, args.NewItems.Count);
+                    tableView.BeginUpdates();
                     tableView.InsertRows(newIndexPaths, AddAnimation);
+                    tableView.EndUpdates();
+                    newIndexPaths.DisposeEx();
                     return true;
                 case NotifyCollectionChangedAction.Remove:
                     NSIndexPath[] oldIndexPaths = PlatformExtensions.CreateNSIndexPathArray(args.OldStartingIndex, args.OldItems.Count);
+                    tableView.BeginUpdates();
                     tableView.DeleteRows(oldIndexPaths, RemoveAnimation);
+                    tableView.EndUpdates();
+                    oldIndexPaths.DisposeEx();
                     return true;
                 case NotifyCollectionChangedAction.Move:
                     if (args.NewItems.Count != 1 && args.OldItems.Count != 1)
@@ -164,13 +170,20 @@ namespace MugenMvvmToolkit.iOS.Binding.Infrastructure
 
                     NSIndexPath oldIndexPath = NSIndexPath.FromRowSection(args.OldStartingIndex, 0);
                     NSIndexPath newIndexPath = NSIndexPath.FromRowSection(args.NewStartingIndex, 0);
+                    tableView.BeginUpdates();
                     tableView.MoveRow(oldIndexPath, newIndexPath);
+                    tableView.EndUpdates();
+                    oldIndexPath.Dispose();
+                    newIndexPath.Dispose();
                     return true;
                 case NotifyCollectionChangedAction.Replace:
                     if (args.NewItems.Count != args.OldItems.Count)
                         return false;
                     NSIndexPath indexPath = NSIndexPath.FromRowSection(args.NewStartingIndex, 0);
+                    tableView.BeginUpdates();
                     tableView.ReloadRows(new[] { indexPath }, ReplaceAnimation);
+                    tableView.EndUpdates();
+                    indexPath.Dispose();
                     return true;
                 default:
                     return false;
