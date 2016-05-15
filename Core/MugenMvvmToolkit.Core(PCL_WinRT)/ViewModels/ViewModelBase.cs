781249e333f33f8dabe114f2c903d37402ae6193
Vyacheslav Volkov <vvs0205@outlook.com>
2016-05-15 14:34:50 +0700

    added InvalidateCommands, CanInvalidateCommands methods

--- Core/MugenMvvmToolkit.Core(PCL_WinRT)/ViewModels/ViewModelBase.cs
+++ Core/MugenMvvmToolkit.Core(PCL_WinRT)/ViewModels/ViewModelBase.cs
@@ -534,6 +534,11 @@ namespace MugenMvvmToolkit.ViewModels
             this.NotBeDisposed();
         }
 
+        protected void InvalidateCommands()
+        {
+            Publish(StateChangedMessage.Empty);
+        }
+
         internal virtual void HandleInternal(object sender, object message)
         {
             var busyToken = message as IBusyToken;
@@ -590,7 +595,8 @@ namespace MugenMvvmToolkit.ViewModels
 
         internal override void OnPropertyChangedInternal(PropertyChangedEventArgs args)
         {
-            Publish(StateChangedMessage.Empty);
+            if (CanInvalidateCommands(args))
+                InvalidateCommands();
         }
 
         #endregion
@@ -614,6 +620,11 @@ namespace MugenMvvmToolkit.ViewModels
         {
             if (InitializeEventAggregator(false))
                 _localEventAggregator.Publish(sender, message);
+        }
+
+        protected virtual bool CanInvalidateCommands(PropertyChangedEventArgs args)
+        {
+            return true;
         }
 
         protected virtual void OnRequestOwnIocContainer()
