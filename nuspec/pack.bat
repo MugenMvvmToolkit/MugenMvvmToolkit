9c341d893fd9e6cd0d042c82ac732fc8026129a5
Vyacheslav Volkov <vvs0205@outlook.com>
2017-03-14 01:25:59 +0700

    updated nuspec

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -2,13 +2,13 @@ del *.nupkg
 nuget pack MugenMvvmToolkit.nuspec
 nuget pack MugenMvvmToolkit.Binding.nuspec
 nuget pack MugenMvvmToolkit.AutofacContainer.nuspec
-nuget pack MugenMvvmToolkit.MugenInjectionContainer.nuspec
 nuget pack MugenMvvmToolkit.NinjectContainer.nuspec
 nuget pack MugenMvvmToolkit.WinForms.nuspec
 nuget pack MugenMvvmToolkit.WPF.nuspec
 nuget pack MugenMvvmToolkit.Xamarin.Forms.nuspec
 nuget pack MugenMvvmToolkit.Android.AppCompat.nuspec
 nuget pack MugenMvvmToolkit.Android.Design.nuspec
+nuget pack MugenMvvmToolkit.Android.Preference.nuspec
 nuget pack MugenMvvmToolkit.Android.RecyclerView.nuspec
 nuget pack MugenMvvmToolkit.StarterPack.nuspec
 nuget pack MugenMvvmToolkit.StarterPack.WPF.nuspec
