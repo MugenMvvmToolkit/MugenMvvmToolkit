c7a8725e308ceba771bfaf084e35febdc5102f87
Vyacheslav Volkov <vvs0205@outlook.com>
2017-03-13 11:48:41 +0700

    moved MugenContainer to core removed MugenContainer project minor changes

--- Core/MugenMvvmToolkit.Core(NetStandard)/Infrastructure/MugenContainer.cs
+++ Core/MugenMvvmToolkit.Core(NetStandard)/Infrastructure/MugenContainer.cs
@@ -26,7 +26,7 @@ using MugenMvvmToolkit.Interfaces.Models;
 using MugenMvvmToolkit.Models;
 using MugenMvvmToolkit.Models.IoC;
 
-namespace MugenMvvmToolkit
+namespace MugenMvvmToolkit.Infrastructure
 {
     public class MugenContainer : IIocContainer
     {
