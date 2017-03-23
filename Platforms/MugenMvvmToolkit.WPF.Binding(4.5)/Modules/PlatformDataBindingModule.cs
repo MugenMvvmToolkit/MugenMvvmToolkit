f7db328b2658b6d40198935ba4b76dd98ea39486
Vyacheslav Volkov <vvs0205@outlook.com>
2017-03-24 00:28:40 +0700

    minor changes

--- Platforms/MugenMvvmToolkit.WPF.Binding(4.5)/Modules/PlatformDataBindingModule.cs
+++ Platforms/MugenMvvmToolkit.WPF.Binding(4.5)/Modules/PlatformDataBindingModule.cs
@@ -144,7 +144,7 @@ namespace MugenMvvmToolkit.UWP.Binding.Modules
                     errorProvider: new WpfBindingErrorProvider(), resourceResolver: new WpfBindingResourceResolver(), converter: BindingConverterExtensions.Convert);
             }
 #elif WINDOWS_UWP
-            if (context.PlatformInfo.Platform == PlatformType.UWP)
+            if (context.PlatformInfo.Platform == PlatformType.UWP || context.PlatformInfo.Platform == PlatformType.UWPPhone)
             {
                 BindingServiceProvider.Initialize(memberProvider: new UwpBindingMemberProvider(), contextManager: new UwpBindingContextManager(),
                     errorProvider: new UwpBindingErrorProvider(), resourceResolver: new UwpBindingResourceResolver(), converter: BindingConverterExtensions.Convert);
