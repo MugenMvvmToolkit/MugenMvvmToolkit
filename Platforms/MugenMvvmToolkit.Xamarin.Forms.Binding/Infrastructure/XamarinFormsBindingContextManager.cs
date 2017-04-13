51c6201208536611102eb88b86f3097ca4a45fc3
Vyacheslav Volkov <vvs0205@outlook.com>
2017-04-14 00:27:43 +0700

    optimized data context

--- Platforms/MugenMvvmToolkit.Xamarin.Forms.Binding/Infrastructure/XamarinFormsBindingContextManager.cs
+++ Platforms/MugenMvvmToolkit.Xamarin.Forms.Binding/Infrastructure/XamarinFormsBindingContextManager.cs
@@ -16,30 +16,82 @@
 
 #endregion
 
-using MugenMvvmToolkit.Binding;
+using System;
+using MugenMvvmToolkit.Binding.DataConstants;
 using MugenMvvmToolkit.Binding.Infrastructure;
 using MugenMvvmToolkit.Binding.Interfaces.Models;
+using MugenMvvmToolkit.Models;
+using Xamarin.Forms;
 
 namespace MugenMvvmToolkit.Xamarin.Forms.Binding.Infrastructure
 {
     public class XamarinFormsBindingContextManager : BindingContextManager
     {
-        #region Fields
+        #region Nested types
 
-        private const string DataContextName = "BindingContext";
+        public sealed class BindableObjectBindingContext : IBindingContext
+        {
+            #region Fields
+
+            private readonly WeakReference _sourceReference;
+
+            #endregion
+
+            #region Constructors
+
+            public BindableObjectBindingContext(BindableObject element)
+            {
+                _sourceReference = ServiceProvider.WeakReferenceFactory(element);
+                element.BindingContextChanged += RaiseDataContextChanged;
+            }
+
+            #endregion
+
+            #region Implementation of IBindingContext
+
+            public object Source => _sourceReference.Target;
+
+            public bool IsAlive => true;
+
+            public object Value
+            {
+                get { return ((BindableObject)Source)?.BindingContext; }
+                set
+                {
+                    var target = (BindableObject)Source;
+                    if (target == null)
+                        return;
+                    if (ReferenceEquals(value, BindingConstants.UnsetValue))
+                        target.ClearValue(BindableObject.BindingContextProperty);
+                    else
+                        target.BindingContext = value;
+                }
+            }
+
+            public event EventHandler<ISourceValue, EventArgs> ValueChanged;
+
+            #endregion
+
+            #region Methods
+
+            private void RaiseDataContextChanged(object sender, EventArgs args)
+            {
+                ValueChanged?.Invoke(this, EventArgs.Empty);
+            }
+
+            #endregion
+        }
 
         #endregion
 
         #region Overrides of BindingContextManager
 
-        protected override IBindingMemberInfo GetExplicitDataContextMember(object source)
+        protected override IBindingContext CreateBindingContext(object item)
         {
-            IBindingMemberInfo member = BindingServiceProvider
-                .MemberProvider
-                .GetBindingMember(source.GetType(), DataContextName, true, false);
-            if (member != null && member.Type.Equals(typeof(object)) && member.CanRead && member.CanWrite)
-                return member;
-            return null;
+            var bindableObject = item as BindableObject;
+            if (bindableObject == null)
+                return base.CreateBindingContext(item);
+            return new BindableObjectBindingContext(bindableObject);
         }
 
         #endregion
