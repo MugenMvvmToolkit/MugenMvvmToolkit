d6d2727d75775bac12ee22073e056477ee97d7c0
Vyacheslav Volkov <vvs0205@outlook.com>
2017-05-22 23:41:37 +0700

    updated nuspec

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -1,21 +1,21 @@
 del *.nupkg
-nuget pack MugenMvvmToolkit.nuspec -symbols
-nuget pack MugenMvvmToolkit.Binding.nuspec -symbols
-nuget pack MugenMvvmToolkit.AutofacContainer.nuspec -symbols
-nuget pack MugenMvvmToolkit.NinjectContainer.nuspec -symbols
-nuget pack MugenMvvmToolkit.WinForms.nuspec -symbols
-nuget pack MugenMvvmToolkit.WPF.nuspec -symbols
-nuget pack MugenMvvmToolkit.WPF.Binding.nuspec -symbols
-nuget pack MugenMvvmToolkit.UWP.nuspec -symbols
-nuget pack MugenMvvmToolkit.UWP.Binding.nuspec -symbols
-nuget pack MugenMvvmToolkit.iOS.nuspec -symbols
-nuget pack MugenMvvmToolkit.Xamarin.Forms.nuspec -symbols
-nuget pack MugenMvvmToolkit.Xamarin.Forms.Binding.nuspec -symbols
-nuget pack MugenMvvmToolkit.Android.nuspec -symbols
-nuget pack MugenMvvmToolkit.Android.AppCompat.nuspec -symbols
-nuget pack MugenMvvmToolkit.Android.Design.nuspec -symbols
-nuget pack MugenMvvmToolkit.Android.Preference.nuspec -symbols
-nuget pack MugenMvvmToolkit.Android.RecyclerView.nuspec -symbols
+nuget pack MugenMvvmToolkit.nuspec
+nuget pack MugenMvvmToolkit.Binding.nuspec
+nuget pack MugenMvvmToolkit.AutofacContainer.nuspec
+nuget pack MugenMvvmToolkit.NinjectContainer.nuspec
+nuget pack MugenMvvmToolkit.WinForms.nuspec
+nuget pack MugenMvvmToolkit.WPF.nuspec
+nuget pack MugenMvvmToolkit.WPF.Binding.nuspec
+nuget pack MugenMvvmToolkit.UWP.nuspec
+nuget pack MugenMvvmToolkit.UWP.Binding.nuspec
+nuget pack MugenMvvmToolkit.iOS.nuspec
+nuget pack MugenMvvmToolkit.Xamarin.Forms.nuspec
+nuget pack MugenMvvmToolkit.Xamarin.Forms.Binding.nuspec
+nuget pack MugenMvvmToolkit.Android.nuspec
+nuget pack MugenMvvmToolkit.Android.AppCompat.nuspec
+nuget pack MugenMvvmToolkit.Android.Design.nuspec
+nuget pack MugenMvvmToolkit.Android.Preference.nuspec
+nuget pack MugenMvvmToolkit.Android.RecyclerView.nuspec
 nuget pack MugenMvvmToolkit.StarterPack.nuspec
 nuget pack MugenMvvmToolkit.StarterPack.WPF.nuspec
 nuget pack MugenMvvmToolkit.StarterPack.WinForms.nuspec
