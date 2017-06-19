db2a24fd2bc17cd1f3083fd6e0c2510396914dab
Vyacheslav Volkov <vvs0205@outlook.com>
2017-06-20 00:39:59 +0700

    added binding support for native views (XamForms)

--- Platforms/MugenMvvmToolkit.Xamarin.Forms.Binding/MarkupExtensions/View.cs
+++ Platforms/MugenMvvmToolkit.Xamarin.Forms.Binding/MarkupExtensions/View.cs
@@ -16,11 +16,13 @@
 
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using MugenMvvmToolkit.Binding;
 using MugenMvvmToolkit.Binding.Infrastructure;
-using MugenMvvmToolkit.Binding.Interfaces;
+using MugenMvvmToolkit.Models;
 using MugenMvvmToolkit.Xamarin.Forms.Binding;
 using Xamarin.Forms;
 
@@ -29,10 +31,25 @@ namespace MugenMvvmToolkit.Xamarin.Forms.MarkupExtensions
 {
     public static class View
     {
-        #region Attached properties
+        #region Constructors
 
-        public static readonly BindableProperty BindProperty = BindableProperty.CreateAttached("Bind", typeof(string),
-            typeof(View), null, propertyChanged: OnBindPropertyChanged);
+        static View()
+        {
+            var nativeViewAccessors = new Dictionary<Type, PropertyInfo>();
+            GetNativeViewHandler = element =>
+            {
+                var type = element?.GetType();
+                if (type == null)
+                    return null;
+                PropertyInfo propertyInfo;
+                if (!nativeViewAccessors.TryGetValue(type, out propertyInfo))
+                {
+                    propertyInfo = element?.GetType()?.GetPropertyEx("NativeView", MemberFlags.Instance | MemberFlags.Public);
+                    nativeViewAccessors[type] = propertyInfo;
+                }
+                return propertyInfo?.GetValue(element);
+            };
+        }
 
         #endregion
 
@@ -48,21 +65,66 @@ namespace MugenMvvmToolkit.Xamarin.Forms.MarkupExtensions
             view.SetValue(BindProperty, value);
         }
 
-        private static void OnBindPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        public static bool GetHasNativeView(BindableObject view)
+        {
+            return (bool)view.GetValue(HasNativeViewProperty);
+        }
+
+        public static void SetHasNativeView(BindableObject view, bool value)
+        {
+            view.SetValue(HasNativeViewProperty, value);
+        }
+
+        private static void OnHasNativeViewChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var element = bindable as Element;
+            if (!(newValue is bool) || element == null)
+                return;
+            if ((bool)newValue)
+                element.ChildAdded += OnChildAdded;
+            else
+                element.ChildAdded -= OnChildAdded;
+        }
+
+        private static void OnChildAdded(object sender, ElementEventArgs args)
+        {
+            var nativeView = GetNativeViewHandler?.Invoke(args.Element);
+            nativeView?.SetBindingMemberValue(AttachedMembers.Object.Parent, args.Element);
+        }
+
+        private static void OnBindPropertyChanged(object bindable, object oldValue, object newValue)
         {
             var bindings = (string)newValue;
             if (string.IsNullOrWhiteSpace(bindings))
                 return;
+            var nativeView = GetNativeViewHandler?.Invoke((BindableObject)bindable);
+            if (nativeView != null)
+            {
+                nativeView.SetBindingMemberValue(AttachedMembers.Object.Parent, bindable);
+                bindable = nativeView;
+            }
             if (XamarinFormsToolkitExtensions.IsDesignMode)
             {
                 XamarinFormsDataBindingExtensions.InitializeFromDesignContext();
-                IList<IDataBinding> list = BindingServiceProvider.BindingProvider.CreateBindingsFromStringWithBindings(bindable, bindings);
-                foreach (InvalidDataBinding binding in list.OfType<InvalidDataBinding>())
+                var list = BindingServiceProvider.BindingProvider.CreateBindingsFromStringWithBindings(bindable, bindings);
+                foreach (var binding in list.OfType<InvalidDataBinding>())
                     throw binding.Exception;
             }
             else
                 BindingServiceProvider.BindingProvider.CreateBindingsFromString(bindable, bindings);
         }
+
+        #endregion
+
+        #region Attached properties
+
+        public static Func<BindableObject, object> GetNativeViewHandler { get; set; }
+
+        public static readonly BindableProperty BindProperty = BindableProperty.CreateAttached("Bind", typeof(string),
+            typeof(BindableObject), null, propertyChanged: OnBindPropertyChanged);
+
+        public static readonly BindableProperty HasNativeViewProperty = BindableProperty.CreateAttached("HasNativeView", typeof(bool),
+            typeof(BindableObject), Empty.FalseObject, propertyChanged: OnHasNativeViewChanged);
 
         #endregion
     }
