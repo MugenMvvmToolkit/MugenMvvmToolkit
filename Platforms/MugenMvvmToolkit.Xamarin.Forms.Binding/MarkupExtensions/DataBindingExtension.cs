7238c6c1b77b79e65feaffe400c99ed476e6bc2d
Vyacheslav Volkov <vvs0205@outlook.com>
2017-10-24 14:01:17 +0700

    removed code to get property name (DataBindingExtension XamForms)

--- Platforms/MugenMvvmToolkit.Xamarin.Forms.Binding/MarkupExtensions/DataBindingExtension.cs
+++ Platforms/MugenMvvmToolkit.Xamarin.Forms.Binding/MarkupExtensions/DataBindingExtension.cs
@@ -17,11 +17,8 @@
 #endregion
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using MugenMvvmToolkit.Binding.Infrastructure;
-using MugenMvvmToolkit.Models;
 using MugenMvvmToolkit.Xamarin.Forms.Binding;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -93,82 +90,7 @@ namespace MugenMvvmToolkit.Xamarin.Forms.MarkupExtensions
             if (memberInfo != null)
                 return memberInfo.Name;
 
-            if (GetTargetPropertyNameDelegate != null)
-                return GetTargetPropertyNameDelegate(provideValueTarget, serviceProvider);
-
-            //Making some reflection magic.
-            var xamlNode = GetValue(provideValueTarget, "Node");
-            if (xamlNode == null)
-            {
-                var xamlNodeProvider = GetValue(serviceProvider, "IXamlNodeProvider");
-                xamlNode = GetValue(xamlNodeProvider, "XamlNode");
-            }
-
-            var properties = (IDictionary)GetValue(GetValue(xamlNode, "Parent"), "Properties");
-            object xmlName = null;
-            if (properties != null)
-            {
-                foreach (DictionaryEntry entry in properties)
-                {
-                    if (ReferenceEquals(entry.Value, xamlNode))
-                    {
-                        xmlName = entry.Key;
-                        break;
-                    }
-                }
-            }
-            return (string)GetValue(xmlName, "LocalName");
-        }
-
-
-        private struct CacheKey
-        {
-            public readonly Type Type;
-            public readonly string Name;
-
-            public CacheKey(Type type, string name)
-            {
-                Type = type;
-                Name = name;
-            }
-
-            private sealed class TypeNameEqualityComparer : IEqualityComparer<CacheKey>
-            {
-                public bool Equals(CacheKey x, CacheKey y)
-                {
-                    return x.Type.Equals(y.Type) && string.Equals(x.Name, y.Name);
-                }
-
-                public int GetHashCode(CacheKey obj)
-                {
-                    unchecked
-                    {
-                        return (obj.Type.GetHashCode() * 397) ^ obj.Name.GetHashCode();
-                    }
-                }
-            }
-
-            public static readonly IEqualityComparer<CacheKey> TypeNameComparerInstance = new TypeNameEqualityComparer();
-        }
-
-        private static readonly Dictionary<CacheKey, Func<object, object>> PropertiesCache = new Dictionary<CacheKey, Func<object, object>>(CacheKey.TypeNameComparerInstance);
-
-        private static object GetValue(object item, string property)
-        {
-            if (item == null)
-                return null;
-            var key = new CacheKey(item.GetType(), property);
-            Func<object, object> func;
-            if (!PropertiesCache.TryGetValue(key, out func))
-            {
-                var propertyInfo = key.Type.GetPropertyEx(property, MemberFlags.Public | MemberFlags.NonPublic | MemberFlags.Instance);
-                if (propertyInfo != null)
-                {
-                    func = ServiceProvider.ReflectionManager.GetMemberGetter<object>(propertyInfo);
-                    PropertiesCache[key] = func;
-                }
-            }
-            return func?.Invoke(item);
+            return GetTargetPropertyNameDelegate?.Invoke(provideValueTarget, serviceProvider);
         }
 
         #endregion
