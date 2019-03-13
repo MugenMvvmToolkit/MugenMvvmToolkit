9d8409dfa7b9244a045cd9c1a6c846ef45e85ed8
Vyacheslav Volkov <vvs0205@outlook.com>
2019-03-13 21:48:51 +0300

    minor changes

--- Tests/MugenMvvm.UnitTest/Collections/ObservableCollectionTestBase.cs
+++ Tests/MugenMvvm.UnitTest/Collections/ObservableCollectionTestBase.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using MugenMvvm.Interfaces.Collections;
-using MugenMvvm.Interfaces.Models;
+using MugenMvvm.UnitTest.TestInfrastructure;
+using MugenMvvm.UnitTest.TestModels;
 using Should;
 using Xunit;
 
@@ -31,7 +29,7 @@ namespace MugenMvvm.UnitTest.Collections
         [Fact]
         public void CreateWithItemsTest()
         {
-            var items = new[] { new CollectionItem(), new CollectionItem() };
+            var items = new[] {new CollectionItem(), new CollectionItem()};
             var collection = CreateCollection(items);
             collection.Count.ShouldEqual(2);
             collection.Any(item => item == items[0]).ShouldBeTrue();
@@ -100,7 +98,7 @@ namespace MugenMvvm.UnitTest.Collections
             var item2 = new CollectionItem();
             var collection = CreateCollection(new CollectionItem());
 
-            collection.Reset(new[] { item1, item2 });
+            collection.Reset(new[] {item1, item2});
             collection[0].ShouldEqual(item1);
             collection[1].ShouldEqual(item2);
             collection.Count.ShouldEqual(2);
@@ -180,7 +178,7 @@ namespace MugenMvvm.UnitTest.Collections
         [Fact]
         public void ClearItemsTest()
         {
-            var items = new[] { new CollectionItem(), new CollectionItem() };
+            var items = new[] {new CollectionItem(), new CollectionItem()};
             var collection = CreateCollection(items);
             collection.Count.ShouldEqual(2);
             collection.Any(item => item == items[0]).ShouldBeTrue();
@@ -194,8 +192,8 @@ namespace MugenMvvm.UnitTest.Collections
         [Fact]
         public void BeginEndBatchUpdateShouldCallListener()
         {
-            int begin = 0;
-            int end = 0;
+            var begin = 0;
+            var end = 0;
 
             var collection = CreateCollection<CollectionItem>();
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -234,8 +232,8 @@ namespace MugenMvvm.UnitTest.Collections
             var expectedIndex = 0;
             var expectedItem = item1;
             var canAdd = false;
-            int adding = 0;
-            int added = 0;
+            var adding = 0;
+            var added = 0;
 
             var collection = CreateCollection<CollectionItem>();
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -295,8 +293,8 @@ namespace MugenMvvm.UnitTest.Collections
             var expectedIndex = 0;
             var expectedItem = item1;
             var canAdd = false;
-            int adding = 0;
-            int added = 0;
+            var adding = 0;
+            var added = 0;
 
             var collection = CreateCollection<CollectionItem>();
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -356,8 +354,8 @@ namespace MugenMvvm.UnitTest.Collections
             var expectedOldItem = item1;
             var expectedNewItem = item2;
             var canReplace = false;
-            int replacing = 0;
-            int replaced = 0;
+            var replacing = 0;
+            var replaced = 0;
 
             var collection = CreateCollection<CollectionItem>(item1, item2);
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -421,8 +419,8 @@ namespace MugenMvvm.UnitTest.Collections
             var expectedOldIndex = 0;
             var expectedNewIndex = 1;
             var canMove = false;
-            int moving = 0;
-            int moved = 0;
+            var moving = 0;
+            var moved = 0;
 
             var collection = CreateCollection<CollectionItem>(item1, item2);
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -484,8 +482,8 @@ namespace MugenMvvm.UnitTest.Collections
             var expectedIndex = 1;
             var expectedItem = item2;
             var canRemove = false;
-            int removing = 0;
-            int removed = 0;
+            var removing = 0;
+            var removed = 0;
 
             var collection = CreateCollection(item1, item2);
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -546,8 +544,8 @@ namespace MugenMvvm.UnitTest.Collections
             var expectedIndex = 1;
             var expectedItem = item2;
             var canRemove = false;
-            int removing = 0;
-            int removed = 0;
+            var removing = 0;
+            var removed = 0;
 
             var collection = CreateCollection(item1, item2);
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -604,12 +602,12 @@ namespace MugenMvvm.UnitTest.Collections
             var item1 = new CollectionItem();
             var item2 = new CollectionItem();
 
-            var resetItems = new[] { item1, item2 };
+            var resetItems = new[] {item1, item2};
 
             var expectedItem = resetItems;
             var canReset = false;
-            int resetting = 0;
-            int reset = 0;
+            var resetting = 0;
+            var reset = 0;
 
             var collection = CreateCollection(new CollectionItem());
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -656,8 +654,8 @@ namespace MugenMvvm.UnitTest.Collections
             var item2 = new CollectionItem();
 
             var canClear = false;
-            int clearing = 0;
-            int cleared = 0;
+            var clearing = 0;
+            var cleared = 0;
 
             var collection = CreateCollection(item1, item2);
             var collectionListener = new CollectionListener<CollectionItem>(collection)
@@ -668,10 +666,7 @@ namespace MugenMvvm.UnitTest.Collections
                     clearing++;
                     return canClear;
                 },
-                OnCleared = _ =>
-                {
-                    cleared++;
-                }
+                OnCleared = _ => { cleared++; }
             };
             if (decorators)
                 collection.DecoratorListeners.Add(collectionListener);
@@ -694,221 +689,6 @@ namespace MugenMvvm.UnitTest.Collections
         }
 
         protected abstract IObservableCollection<T> CreateCollection<T>(params T[] items) where T : class;
-
-        #endregion
-
-        #region Nested types
-
-        public class CollectionListener<T> : IObservableCollectionChangingListener<T>
-        {
-            #region Fields
-
-            private readonly IObservableCollection<T> _collection;
-
-            #endregion
-
-            #region Constructors
-
-            public CollectionListener(IObservableCollection<T> collection)
-            {
-                _collection = collection;
-            }
-
-            #endregion
-
-            #region Properties
-
-            public Func<IObservableCollection<T>, T, int, bool> OnAdding { get; set; }
-
-            public Func<IObservableCollection<T>, T, T, int, bool> OnReplacing { get; set; }
-
-            public Func<IObservableCollection<T>, T, int, int, bool> OnMoving { get; set; }
-
-            public Func<IObservableCollection<T>, T, int, bool> OnRemoving { get; set; }
-
-            public Func<IObservableCollection<T>, IEnumerable<T>, bool> OnResetting { get; set; }
-
-            public Func<IObservableCollection<T>, bool> OnClearing { get; set; }
-
-            public Action<IObservableCollection<T>> OnBeginBatchUpdate { get; set; }
-
-            public Action<IObservableCollection<T>> OnEndBatchUpdate { get; set; }
-
-            public Action<IObservableCollection<T>, T, int> OnAdded { get; set; }
-
-            public Action<IObservableCollection<T>, T, T, int> OnReplaced { get; set; }
-
-            public Action<IObservableCollection<T>, T, int, int> OnMoved { get; set; }
-
-            public Action<IObservableCollection<T>, T, int> OnRemoved { get; set; }
-
-            public Action<IObservableCollection<T>, IEnumerable<T>> OnReset { get; set; }
-
-            public Action<IObservableCollection<T>> OnCleared { get; set; }
-
-            public bool ThrowErrorNullDelegate { get; set; }
-
-            #endregion
-
-            #region Implementation of interfaces
-
-            int IListener.GetPriority(object source)
-            {
-                return 0;
-            }
-
-            void IObservableCollectionChangedListener<T>.OnBeginBatchUpdate(IObservableCollection<T> collection)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnBeginBatchUpdate == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnBeginBatchUpdate?.Invoke(collection);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnEndBatchUpdate(IObservableCollection<T> collection)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnEndBatchUpdate == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnEndBatchUpdate?.Invoke(collection);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnAdded(IObservableCollection<T> collection, T item, int index)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnAdded == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnAdded?.Invoke(collection, item, index);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnReplaced(IObservableCollection<T> collection, T oldItem, T newItem, int index)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnReplaced == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnReplaced?.Invoke(collection, oldItem, newItem, index);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnMoved(IObservableCollection<T> collection, T item, int oldIndex, int newIndex)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnMoved == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnMoved?.Invoke(collection, item, oldIndex, newIndex);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnRemoved(IObservableCollection<T> collection, T item, int index)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnRemoved == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnRemoved?.Invoke(collection, item, index);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnReset(IObservableCollection<T> collection, IEnumerable<T> items)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnReset == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnReset?.Invoke(collection, items);
-            }
-
-            void IObservableCollectionChangedListener<T>.OnCleared(IObservableCollection<T> collection)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnCleared == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                OnCleared?.Invoke(collection);
-            }
-
-            bool IObservableCollectionChangingListener<T>.OnAdding(IObservableCollection<T> collection, T item, int index)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnAdding == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                return OnAdding?.Invoke(collection, item, index) ?? true;
-            }
-
-            bool IObservableCollectionChangingListener<T>.OnReplacing(IObservableCollection<T> collection, T oldItem, T newItem, int index)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnReplacing == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                return OnReplacing?.Invoke(collection, oldItem, newItem, index) ?? true;
-            }
-
-            bool IObservableCollectionChangingListener<T>.OnMoving(IObservableCollection<T> collection, T item, int oldIndex, int newIndex)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnMoving == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                return OnMoving?.Invoke(collection, item, oldIndex, newIndex) ?? true;
-            }
-
-            bool IObservableCollectionChangingListener<T>.OnRemoving(IObservableCollection<T> collection, T item, int index)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnRemoving == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                return OnRemoving?.Invoke(collection, item, index) ?? true;
-            }
-
-            bool IObservableCollectionChangingListener<T>.OnResetting(IObservableCollection<T> collection, IEnumerable<T> items)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnResetting == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                return OnResetting?.Invoke(collection, items) ?? true;
-            }
-
-            bool IObservableCollectionChangingListener<T>.OnClearing(IObservableCollection<T> collection)
-            {
-                _collection.ShouldEqual(collection);
-                if (OnClearing == null && ThrowErrorNullDelegate)
-                    throw new NotSupportedException();
-                return OnClearing?.Invoke(collection) ?? true;
-            }
-
-            #endregion
-        }
-
-        [DebuggerDisplay("Id = {Id}")]
-        public sealed class CollectionItem
-        {
-            #region Fields
-
-            private static int _idGenerator = -1;
-
-            #endregion
-
-            #region Constructors
-
-            public CollectionItem()
-            {
-                Id = Interlocked.Increment(ref _idGenerator);
-            }
-
-            #endregion
-
-            #region Properties
-
-            public bool Hidden { get; set; }
-
-            public string Name { get; set; }
-
-            public int Id { get; set; }
-
-            #endregion
-
-            #region Methods
-
-            public override string ToString()
-            {
-                return $"Id: {Id}";
-            }
-
-            #endregion
-        }
 
         #endregion
     }
---
+++ Tests/MugenMvvm.UnitTest/TestModels/CollectionItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace MugenMvvm.UnitTest.TestModels
+{
+    [DebuggerDisplay("Id = {Id}")]
+    public class CollectionItem
+    {
+        #region Fields
+
+        private static int _idGenerator = -1;
+
+        #endregion
+
+        #region Constructors
+
+        public CollectionItem()
+        {
+            Id = Interlocked.Increment(ref _idGenerator);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool Hidden { get; set; }
+
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"Id: {Id}";
+        }
+
+        #endregion
+    }
+}
