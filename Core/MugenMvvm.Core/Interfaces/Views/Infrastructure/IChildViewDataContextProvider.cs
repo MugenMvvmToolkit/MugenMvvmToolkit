6c4494492b76b6f587a091572a3ed563fd9040b2
Vyacheslav Volkov <vvs0205@outlook.com>
2019-04-26 21:09:58 +0300

    added IChildViewDataContextProvider, ViewDataContextProvider

---
+++ Core/MugenMvvm.Core/Interfaces/Views/Infrastructure/IChildViewDataContextProvider.cs
@@ -0,0 +1,12 @@
+﻿using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+
+namespace MugenMvvm.Interfaces.Views.Infrastructure
+{
+    public interface IChildViewDataContextProvider : IHasPriority
+    {
+        bool TryGetDataContext(object view, IReadOnlyMetadataContext metadata, out object? dataContext);
+
+        bool TrySetDataContext(object view, object? dataContext, IReadOnlyMetadataContext metadata);
+    }
+}
--- Core/MugenMvvm.Core/Interfaces/Views/Infrastructure/IViewDataContextProvider.cs
+++ Core/MugenMvvm.Core/Interfaces/Views/Infrastructure/IViewDataContextProvider.cs
@@ -1,11 +1,14 @@
-﻿using MugenMvvm.Interfaces.Metadata;
+﻿using MugenMvvm.Interfaces.Components;
+using MugenMvvm.Interfaces.Metadata;
 
 namespace MugenMvvm.Interfaces.Views.Infrastructure
 {
-    public interface IViewDataContextProvider//todo listeners
+    public interface IViewDataContextProvider
     {
+        IComponentCollection<IChildViewDataContextProvider> Providers { get; }
+
         object? GetDataContext(object view, IReadOnlyMetadataContext metadata);
 
-        void SetDataContext(object view, object? dataContext, IReadOnlyMetadataContext metadata);
+        bool SetDataContext(object view, object? dataContext, IReadOnlyMetadataContext metadata);
     }
 }
