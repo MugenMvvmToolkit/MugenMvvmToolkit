2014b3004cf37b8702b765b6664925703cb46b80
Vyacheslav Volkov <vvs0205@outlook.com>
2019-09-20 20:15:33 +0200

    removed tests

--- Tests/MugenMvvm.UnitTest/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MugenMvvm.UnitTest
-{
-    public delegate TResult FuncRef<T1, T2, T3, out TResult>(ref T1 arg1, ref T2 arg2, ref T3 arg3);
-
-    public delegate TResult FuncRef<T1, T2, out TResult>(ref T1 arg1, ref T2 arg2);
-
-    public delegate TResult FuncRef<T1, out TResult>(ref T1 arg1);
-}
