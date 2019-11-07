6ebca2176ba849636331a9bf8024173b4eab2289
Vyacheslav Volkov <vvs0205@outlook.com>
2019-11-07 10:05:36 +0300

    updated IMugenApplication removed IApplicationStateDispatcher minor changes

---
+++ Core/MugenMvvm.Core/Interfaces/App/Components/IApplicationStateListener.cs
@@ -0,0 +1,11 @@
+﻿using MugenMvvm.Enums;
+using MugenMvvm.Interfaces.Components;
+using MugenMvvm.Interfaces.Metadata;
+
+namespace MugenMvvm.Interfaces.App.Components
+{
+    public interface IApplicationStateListener : IComponent<IMugenApplication>
+    {
+        void OnStateChanged(IMugenApplication application, ApplicationState oldState, ApplicationState newState, IReadOnlyMetadataContext? metadata);
+    }
+}
--- Core/MugenMvvm.Core/Interfaces/App/IMugenApplication.cs
+++ Core/MugenMvvm.Core/Interfaces/App/IMugenApplication.cs
@@ -1,8 +1,13 @@
-﻿using MugenMvvm.Interfaces.Components;
+﻿using MugenMvvm.Enums;
+using MugenMvvm.Interfaces.Components;
+using MugenMvvm.Interfaces.Metadata;
 
 namespace MugenMvvm.Interfaces.App
 {
     public interface IMugenApplication : IComponentOwner<IMugenApplication>
     {
+        ApplicationState State { get; }
+
+        void SetApplicationState(ApplicationState state, IReadOnlyMetadataContext? metadata = null);
     }
 }
