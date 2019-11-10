ca0a66ec418ceef910d55398919946ec6f0cc270
Vyacheslav Volkov <vvs0205@outlook.com>
2019-11-10 19:58:45 +0300

    minor changes

--- Core/MugenMvvm.Core/Interfaces/App/IMugenApplication.cs
+++ Core/MugenMvvm.Core/Interfaces/App/IMugenApplication.cs
@@ -1,11 +1,14 @@
 ﻿using MugenMvvm.Enums;
 using MugenMvvm.Interfaces.Components;
 using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
 
 namespace MugenMvvm.Interfaces.App
 {
-    public interface IMugenApplication : IComponentOwner<IMugenApplication>
+    public interface IMugenApplication : IComponentOwner<IMugenApplication>, IMetadataOwner<IMetadataContext>
     {
+        IDeviceInfo DeviceInfo { get; }
+
         ApplicationState State { get; }
 
         void SetApplicationState(ApplicationState state, IReadOnlyMetadataContext? metadata = null);
