552a21fdf4a57b99b4218b00484e706fe168ed42
Vyacheslav Volkov <vvs0205@outlook.com>
2019-11-22 12:24:38 +0300

    added DefaultBindingComponentProviderComponent

---
+++ Core/MugenMvvm.Binding/Core/Components/DefaultBindingComponentProviderComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using MugenMvvm.Binding.Interfaces.Core;
+using MugenMvvm.Binding.Interfaces.Core.Components;
+using MugenMvvm.Binding.Interfaces.Parsing.Expressions;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.Binding.Core.Components
+{
+    public sealed class DefaultBindingComponentProviderComponent : IBindingComponentProviderComponent, IHasPriority
+    {
+        #region Constructors
+
+        public DefaultBindingComponentProviderComponent()
+        {
+            DefaultBindingComponents = new List<IBindingComponentBuilder>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Priority { get; set; } = int.MaxValue;
+
+        public List<IBindingComponentBuilder> DefaultBindingComponents { get; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public ItemOrList<IBindingComponentBuilder?, IReadOnlyList<IBindingComponentBuilder>> TryGetComponentBuilders(IExpressionNode targetExpression, IExpressionNode sourceExpression,
+            ItemOrList<IExpressionNode?, IReadOnlyList<IExpressionNode>> parameters, IReadOnlyMetadataContext? metadata)
+        {
+            return DefaultBindingComponents;
+        }
+
+        #endregion
+    }
+}
