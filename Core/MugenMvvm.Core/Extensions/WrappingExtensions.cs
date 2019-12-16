3cf2ff960efd02b7d540773765f7293769769870
Vyacheslav Volkov <vvs0205@outlook.com>
2019-12-16 11:58:51 +0300

    minor changes

--- Core/MugenMvvm.Core/Extensions/WrappingExtensions.cs
+++ Core/MugenMvvm.Core/Extensions/WrappingExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Linq;
-using MugenMvvm.Enums;
-using MugenMvvm.Interfaces.Internal;
+using MugenMvvm.Constants;
 using MugenMvvm.Interfaces.Metadata;
 using MugenMvvm.Interfaces.Wrapping;
 using MugenMvvm.Interfaces.Wrapping.Components;
@@ -13,57 +11,13 @@ namespace MugenMvvm.Extensions
     {
         #region Methods
 
-        public static IWrapperManagerComponent AddWrapper(this IWrapperManager wrapperManager, Func<Type, Type, IReadOnlyMetadataContext?, bool> condition,
-            Func<object, Type, IReadOnlyMetadataContext?, object?> wrapperFactory, IReadOnlyMetadataContext? metadata = null)
+        public static IWrapperManagerComponent AddWrapper<TState>(this IWrapperManager wrapperManager, TState state, Func<Type, Type, TState, IReadOnlyMetadataContext?, bool> condition,
+            Func<object, Type, TState, IReadOnlyMetadataContext?, object?> wrapperFactory, int priority = WrappingComponentPriority.WrapperManger, IReadOnlyMetadataContext? metadata = null)
         {
             Should.NotBeNull(wrapperManager, nameof(wrapperManager));
-            var factory = new DelegateWrapperManagerComponent(condition, wrapperFactory);
+            var factory = new DelegateWrapperManagerComponent<TState>(condition, wrapperFactory, state) {Priority = priority};
             wrapperManager.Components.Add(factory, metadata);
             return factory;
-        }
-
-        public static IWrapperManagerComponent AddWrapper(this IWrapperManager wrapperManager, Type wrapperType, Type implementation,
-            Func<object, Type, IReadOnlyMetadataContext?, object>? wrapperFactory = null,
-            IReflectionDelegateProvider? reflectionDelegateProvider = null)
-        {
-            Should.NotBeNull(wrapperManager, nameof(wrapperManager));
-            Should.NotBeNull(wrapperType, nameof(wrapperType));
-            Should.BeOfType(implementation, nameof(implementation), wrapperType);
-            if (implementation.IsInterface || implementation.IsAbstract)
-                ExceptionManager.ThrowWrapperTypeShouldBeNonAbstract(implementation);
-
-            if (wrapperFactory == null)
-            {
-                var constructor = implementation
-                    .GetConstructors(BindingFlagsEx.InstanceOnly)
-                    .FirstOrDefault(info => info.GetParameters().Length == 1);
-                if (constructor == null)
-                    ExceptionManager.ThrowCannotFindConstructor(implementation);
-
-                wrapperFactory = constructor.GetActivator<Func<object, object>>(reflectionDelegateProvider).Invoke;
-            }
-
-            return wrapperManager.AddWrapper((_, requestedWrapperType, __) => wrapperType == requestedWrapperType, wrapperFactory); //todo closure check
-        }
-
-        public static IWrapperManagerComponent AddWrapper<TWrapper>(this IWrapperManager wrapperManager, Type implementation,
-            Func<object, Type, IReadOnlyMetadataContext?, TWrapper>? wrapperFactory = null)
-            where TWrapper : class
-        {
-            return wrapperManager.AddWrapper(typeof(TWrapper), implementation, wrapperFactory);
-        }
-
-        public static IWrapperManagerComponent AddWrapper<TWrapper, TImplementation>(this IWrapperManager wrapperManager,
-            Func<object, Type, IReadOnlyMetadataContext?, TWrapper>? wrapperFactory = null)
-            where TWrapper : class
-            where TImplementation : class, TWrapper
-        {
-            return wrapperManager.AddWrapper(typeof(TWrapper), typeof(TImplementation), wrapperFactory);
-        }
-
-        private static object Invoke(this Func<object, object> activator, object target, Type _, IReadOnlyMetadataContext? __)
-        {
-            return activator(target);
         }
 
         #endregion
--- Core/MugenMvvm.Core/Wrapping/Components/DelegateWrapperManagerComponent.cs
+++ Core/MugenMvvm.Core/Wrapping/Components/DelegateWrapperManagerComponent.cs
@@ -6,23 +6,26 @@ using MugenMvvm.Interfaces.Wrapping.Components;
 
 namespace MugenMvvm.Wrapping.Components
 {
-    public sealed class DelegateWrapperManagerComponent : IWrapperManagerComponent, IHasPriority
+    public sealed class DelegateWrapperManagerComponent<TState> : IWrapperManagerComponent, IHasPriority
     {
         #region Fields
 
-        public Func<Type, Type, IReadOnlyMetadataContext?, bool> Condition;
-        public Func<object, Type, IReadOnlyMetadataContext?, object?> WrapperFactory;
+        private readonly Func<Type, Type, TState, IReadOnlyMetadataContext?, bool> _condition;
+        private readonly TState _state;
+        private readonly Func<object, Type, TState, IReadOnlyMetadataContext?, object?> _wrapperFactory;
 
         #endregion
 
         #region Constructors
 
-        public DelegateWrapperManagerComponent(Func<Type, Type, IReadOnlyMetadataContext?, bool> condition, Func<object, Type, IReadOnlyMetadataContext?, object?> wrapperFactory)
+        public DelegateWrapperManagerComponent(Func<Type, Type, TState, IReadOnlyMetadataContext?, bool> condition,
+            Func<object, Type, TState, IReadOnlyMetadataContext?, object?> wrapperFactory, TState state)
         {
             Should.NotBeNull(condition, nameof(condition));
             Should.NotBeNull(wrapperFactory, nameof(wrapperFactory));
-            Condition = condition;
-            WrapperFactory = wrapperFactory;
+            _condition = condition;
+            _wrapperFactory = wrapperFactory;
+            _state = state;
         }
 
         #endregion
@@ -37,12 +40,12 @@ namespace MugenMvvm.Wrapping.Components
 
         public bool CanWrap(Type targetType, Type wrapperType, IReadOnlyMetadataContext? metadata)
         {
-            return Condition(targetType, wrapperType, metadata);
+            return _condition(targetType, wrapperType, _state, metadata);
         }
 
         public object? TryWrap(object target, Type wrapperType, IReadOnlyMetadataContext? metadata)
         {
-            return WrapperFactory(target, wrapperType, metadata);
+            return _wrapperFactory(target, wrapperType, _state, metadata);
         }
 
         #endregion
