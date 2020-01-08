e4028e3015f21fc6fee13a34b95623122fd6b56a
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-08 12:26:17 +0100

    minor changes

--- Core/MugenMvvm.Core/Delegates/FuncIn.cs
+++ Core/MugenMvvm.Core/Delegates/FuncIn.cs
@@ -1,6 +1,6 @@
 ﻿namespace MugenMvvm.Delegates
 {
-    public delegate TReturn FuncIn<T1, TReturn>(in T1 arg1);
+    public delegate TReturn FuncIn<T1, TReturn>(in T1 arg1);//todo remove
 
     public delegate TReturn FuncIn<T1, T2, TReturn>(in T1 arg1, T2 arg2);
 
