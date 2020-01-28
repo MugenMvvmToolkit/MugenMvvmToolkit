b06e3d29cd1e3ca8b57f8fb9768b597b88a8a47f
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-28 15:41:34 +0100

    minor changes

--- Core/MugenMvvm.Core/Internal/Components/WeakReferenceProviderComponent.cs
+++ Core/MugenMvvm.Core/Internal/Components/WeakReferenceProviderComponent.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using MugenMvvm.Constants;
+﻿using MugenMvvm.Constants;
 using MugenMvvm.Interfaces.Internal;
 using MugenMvvm.Interfaces.Internal.Components;
 using MugenMvvm.Interfaces.Metadata;
@@ -22,30 +21,6 @@ namespace MugenMvvm.Internal.Components
         public IWeakReference? TryGetWeakReference(object item, IReadOnlyMetadataContext? metadata)
         {
             return new WeakReferenceImpl(item, TrackResurrection);
-        }
-
-        #endregion
-
-        #region Nested types
-
-        private sealed class WeakReferenceImpl : WeakReference, IWeakReference
-        {
-            #region Constructors
-
-            public WeakReferenceImpl(object target, bool trackResurrection) : base(target, trackResurrection)
-            {
-            }
-
-            #endregion
-
-            #region Implementation of interfaces
-
-            public void Release()
-            {
-                Target = null;
-            }
-
-            #endregion
         }
 
         #endregion
---
+++ Core/MugenMvvm.Core/Internal/WeakReferenceImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MugenMvvm.Interfaces.Internal;
+
+namespace MugenMvvm.Internal
+{
+    public sealed class WeakReferenceImpl : WeakReference, IWeakReference
+    {
+        #region Constructors
+
+        public WeakReferenceImpl(object target, bool trackResurrection) : base(target, trackResurrection)
+        {
+        }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public void Release()
+        {
+            Target = null;
+        }
+
+        #endregion
+    }
+}
