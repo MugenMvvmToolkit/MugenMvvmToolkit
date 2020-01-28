8dff683f878700ae7c93cce17c178e7fad3c6f13
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-28 15:13:38 +0100

    added ReflectionDelegateProviderTest minor changes

---
+++ Tests/MugenMvvm.UnitTest/Internal/TestMemberReflectionDelegateProviderComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using MugenMvvm.Interfaces.Internal.Components;
+using MugenMvvm.Interfaces.Models;
+
+namespace MugenMvvm.UnitTest.Internal
+{
+    public class TestMemberReflectionDelegateProviderComponent : IMemberReflectionDelegateProviderComponent, IHasPriority
+    {
+        #region Properties
+
+        public Func<MemberInfo, Type, Delegate?>? TryGetMemberGetter { get; set; }
+
+        public Func<MemberInfo, Type, Delegate?>? TryGetMemberSetter { get; set; }
+
+        public int Priority { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        Delegate? IMemberReflectionDelegateProviderComponent.TryGetMemberGetter(MemberInfo member, Type delegateType)
+        {
+            return TryGetMemberGetter?.Invoke(member, delegateType);
+        }
+
+        Delegate? IMemberReflectionDelegateProviderComponent.TryGetMemberSetter(MemberInfo member, Type delegateType)
+        {
+            return TryGetMemberSetter?.Invoke(member, delegateType);
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Internal/TestMethodReflectionDelegateProviderComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using MugenMvvm.Interfaces.Internal.Components;
+using MugenMvvm.Interfaces.Models;
+
+namespace MugenMvvm.UnitTest.Internal
+{
+    public class TestMethodReflectionDelegateProviderComponent : IMethodReflectionDelegateProviderComponent, IHasPriority
+    {
+        #region Properties
+
+        public Func<MethodInfo, Func<object?, object?[], object?>?>? TryGetMethodInvoker { get; set; }
+
+        public Func<MethodInfo, Type, Delegate?>? TryGetMethodInvoker1 { get; set; }
+
+        public int Priority { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        Func<object?, object?[], object?>? IMethodReflectionDelegateProviderComponent.TryGetMethodInvoker(MethodInfo method)
+        {
+            return TryGetMethodInvoker?.Invoke(method);
+        }
+
+        Delegate? IMethodReflectionDelegateProviderComponent.TryGetMethodInvoker(MethodInfo method, Type delegateType)
+        {
+            return TryGetMethodInvoker1?.Invoke(method, delegateType);
+        }
+
+        #endregion
+    }
+}
