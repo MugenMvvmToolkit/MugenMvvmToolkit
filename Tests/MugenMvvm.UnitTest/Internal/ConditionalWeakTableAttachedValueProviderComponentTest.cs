838971ce17e90523fb1ee4c96197606e0ef00119
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-28 17:58:26 +0100

    added AttachedValueProviderComponentTestBase, ConditionalWeakTableAttachedValueProviderComponentTest, ValueHolderAttachedValueProviderComponentTest minor changes

---
+++ Tests/MugenMvvm.UnitTest/Internal/ConditionalWeakTableAttachedValueProviderComponentTest.cs
@@ -0,0 +1,22 @@
+﻿using MugenMvvm.Interfaces.Internal.Components;
+using MugenMvvm.Internal.Components;
+
+namespace MugenMvvm.UnitTest.Internal
+{
+    public class ConditionalWeakTableAttachedValueProviderComponentTest : AttachedValueProviderComponentTestBase
+    {
+        #region Methods
+
+        protected override object GetSupportedItem()
+        {
+            return new object();
+        }
+
+        protected override IAttachedValueProviderComponent GetComponent()
+        {
+            return new ConditionalWeakTableAttachedValueProviderComponent();
+        }
+
+        #endregion
+    }
+}
