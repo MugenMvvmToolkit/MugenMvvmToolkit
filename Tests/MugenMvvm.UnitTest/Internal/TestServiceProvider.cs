303991145f85e0697de946f90477c544926be62e
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-28 13:25:31 +0100

    added FallbackServiceConfigurationTest minor changes

---
+++ Tests/MugenMvvm.UnitTest/Internal/TestServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MugenMvvm.UnitTest.Internal
+{
+    public class TestServiceProvider : IServiceProvider
+    {
+        #region Properties
+
+        public Func<Type, object>? GetService { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        object IServiceProvider.GetService(Type serviceType)
+        {
+            return GetService?.Invoke(serviceType)!;
+        }
+
+        #endregion
+    }
+}
