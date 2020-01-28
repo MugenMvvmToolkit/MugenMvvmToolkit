ab3bb70cd2be4453a5ece09435011a229643836c
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-28 15:44:58 +0100

    added WeakReferenceImplTest

---
+++ Tests/MugenMvvm.UnitTest/Internal/WeakReferenceImplTest.cs
@@ -0,0 +1,23 @@
+﻿using MugenMvvm.Internal;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Internal
+{
+    public class WeakReferenceImplTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ReleaseShouldClearReference()
+        {
+            var weakReferenceImpl = new WeakReferenceImpl(this, true);
+            weakReferenceImpl.Target.ShouldNotBeNull();
+
+            weakReferenceImpl.Release();
+            weakReferenceImpl.Target.ShouldBeNull();
+        }
+
+        #endregion
+    }
+}
