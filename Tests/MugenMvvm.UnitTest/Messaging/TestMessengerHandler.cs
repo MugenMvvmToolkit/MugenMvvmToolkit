dd5ef433aa2de3c5c52512056a8709ae211fecca
Vyacheslav Volkov <vvs0205@outlook.com>
2020-01-29 18:23:45 +0100

    added MessengerHandlerSubscriberComponentTest minor changes

---
+++ Tests/MugenMvvm.UnitTest/Messaging/TestMessengerHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MugenMvvm.Interfaces.Messaging;
+
+namespace MugenMvvm.UnitTest.Messaging
+{
+    public class TestMessengerHandler : IMessengerHandler<string>, IMessengerHandler<int>
+    {
+        #region Properties
+
+        public Action<int, IMessageContext>? HandleInt { get; set; }
+
+        public Action<string, IMessageContext>? HandleString { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        void IMessengerHandler<int>.Handle(int message, IMessageContext messageContext)
+        {
+            HandleInt?.Invoke(message, messageContext);
+        }
+
+        void IMessengerHandler<string>.Handle(string message, IMessageContext messageContext)
+        {
+            HandleString?.Invoke(message, messageContext);
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Messaging/TestMessengerHandlerGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MugenMvvm.Interfaces.Messaging;
+
+namespace MugenMvvm.UnitTest.Messaging
+{
+    public class TestMessengerHandlerGeneric<T> : IMessengerHandler<T>
+    {
+        #region Properties
+
+        public Action<object, Type, IMessageContext>? Handle { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        void IMessengerHandler<T>.Handle(T message, IMessageContext messageContext)
+        {
+            Handle?.Invoke(message!, typeof(T), messageContext);
+        }
+
+        #endregion
+    }
+}
