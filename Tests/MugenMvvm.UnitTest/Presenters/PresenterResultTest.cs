5226942d1db07c47d9c50b3341ded10d4dfdb023
Vyacheslav Volkov <vvs0205@outlook.com>
2020-02-07 13:59:48 +0100

    added PresenterResultTest

---
+++ Tests/MugenMvvm.UnitTest/Presenters/PresenterResultTest.cs
@@ -0,0 +1,36 @@
+﻿using MugenMvvm.Enums;
+using MugenMvvm.Presenters;
+using MugenMvvm.UnitTest.Navigation;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Presenters
+{
+    public class PresenterResultTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void IsEmptyShouldBeTrueForDefault()
+        {
+            PresenterResult result = default;
+            result.IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            var id = "test";
+            var provider = new TestNavigationProvider();
+            var navigationType = NavigationType.Alert;
+            var presenterResult = new PresenterResult(id, provider, navigationType, DefaultMetadata);
+            presenterResult.IsEmpty.ShouldBeFalse();
+            presenterResult.NavigationType.ShouldEqual(navigationType);
+            presenterResult.NavigationOperationId.ShouldEqual(id);
+            presenterResult.NavigationProvider.ShouldEqual(provider);
+            presenterResult.Metadata.ShouldEqual(DefaultMetadata);
+        }
+
+        #endregion
+    }
+}
