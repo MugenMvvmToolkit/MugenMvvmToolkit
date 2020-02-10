dea2dc57a613780c54c7f7b4fe4c523e416b0fb5
Vyacheslav Volkov <vvs0205@outlook.com>
2020-02-10 17:25:22 +0100

    added SerializationContextTest

---
+++ Tests/MugenMvvm.UnitTest/Serialization/SerializationContextTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Serialization;
+using MugenMvvm.UnitTest.Metadata;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Serialization
+{
+    public class SerializationContextTest : MetadataOwnerTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void BeginShouldThrowDoubleInitialization()
+        {
+            var ctx = new SerializationContext();
+            using var t = SerializationContext.Begin(ctx);
+            ShouldThrow<InvalidOperationException>(() => SerializationContext.Begin(ctx));
+        }
+
+        [Fact]
+        public void BeginShouldInitializeContextForThread()
+        {
+            var ctx = new SerializationContext();
+            SerializationContext.Current.ShouldBeNull();
+            var actionToken = SerializationContext.Begin(ctx);
+            SerializationContext.Current.ShouldEqual(ctx);
+
+            Task.Run(() => { SerializationContext.Current.ShouldBeNull(); }).Wait();
+
+            actionToken.Dispose();
+            SerializationContext.Current.ShouldBeNull();
+        }
+
+        protected override IMetadataOwner<IMetadataContext> GetMetadataOwner(IReadOnlyMetadataContext? metadata, IMetadataContextProvider? metadataContextProvider)
+        {
+            return new SerializationContext(metadata, metadataContextProvider);
+        }
+
+        #endregion
+    }
+}
