353bb266ab78427a816ecf83a9d172458616611e
Vyacheslav Volkov <vvs0205@outlook.com>
2020-02-11 16:43:56 +0100

    added ViewModelViewMappingTest

---
+++ Tests/MugenMvvm.UnitTest/Views/ViewModelViewMappingTest.cs
@@ -0,0 +1,27 @@
+﻿using MugenMvvm.UnitTest.ViewModels;
+using MugenMvvm.Views;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Views
+{
+    public class ViewModelViewMappingTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            var id = "id";
+            var viewType = typeof(string);
+            var viewModelType = typeof(TestViewModel);
+            var mapping = new ViewModelViewMapping(id, viewType, viewModelType, DefaultMetadata);
+            mapping.Metadata.ShouldEqual(DefaultMetadata);
+            mapping.Id.ShouldEqual(id);
+            mapping.ViewType.ShouldEqual(viewType);
+            mapping.ViewModelType.ShouldEqual(viewModelType);
+        }
+
+        #endregion
+    }
+}
