a32d317a1bc2ca6af42657cb64190766d589ead3
Vyacheslav Volkov <vvs0205@outlook.com>
2020-02-12 15:13:20 +0100

    added AggregatorValidatorProviderComponent minor changes

---
+++ Core/MugenMvvm.Core/Constants/ValidationComponentPriority.cs
@@ -0,0 +1,12 @@
+﻿namespace MugenMvvm.Constants
+{
+    public static class ValidationComponentPriority
+    {
+        #region Fields
+
+        public const int AggregatorProvider = 0;
+        public const int ValidatorProvider = 0;
+
+        #endregion
+    }
+}
---
+++ Core/MugenMvvm.Core/Validation/Components/AggregatorValidatorProviderComponent.cs
@@ -0,0 +1,26 @@
+﻿using MugenMvvm.Constants;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.Validation;
+using MugenMvvm.Interfaces.Validation.Components;
+
+namespace MugenMvvm.Validation.Components
+{
+    public sealed class AggregatorValidatorProviderComponent : IAggregatorValidatorProviderComponent, IHasPriority
+    {
+        #region Properties
+
+        public int Priority { get; set; } = ValidationComponentPriority.AggregatorProvider;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public IAggregatorValidator? TryGetAggregatorValidator<TRequest>(in TRequest request, IReadOnlyMetadataContext? metadata)
+        {
+            return new AggregatorValidator();
+        }
+
+        #endregion
+    }
+}
