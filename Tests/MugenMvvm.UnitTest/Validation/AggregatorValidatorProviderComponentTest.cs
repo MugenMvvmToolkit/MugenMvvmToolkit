615a9dc1d59d9c5b79eed6fd22ba7be50509d96a
Vyacheslav Volkov <vvs0205@outlook.com>
2020-02-12 15:39:06 +0100

    added AggregatorValidatorProviderComponentTest

---
+++ Tests/MugenMvvm.UnitTest/Validation/AggregatorValidatorProviderComponentTest.cs
@@ -0,0 +1,20 @@
+﻿using MugenMvvm.Validation;
+using MugenMvvm.Validation.Components;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Validation
+{
+    public class AggregatorValidatorProviderComponentTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void TryGetAggregatorValidatorShouldReturnAggregatorValidator()
+        {
+            new AggregatorValidatorProviderComponent().TryGetAggregatorValidator(this, DefaultMetadata).ShouldBeType<AggregatorValidator>();
+        }
+
+        #endregion
+    }
+}
