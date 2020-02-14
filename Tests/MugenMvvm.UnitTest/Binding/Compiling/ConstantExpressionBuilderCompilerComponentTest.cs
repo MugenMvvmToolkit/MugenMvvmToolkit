843df7d6d921c6b8d92a5c95d8db36ea3387ee65
Vyacheslav Volkov <vvs0205@outlook.com>
2020-02-14 12:15:21 +0100

    added ConstantExpressionBuilderCompilerComponentTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Compiling/ConstantExpressionBuilderCompilerComponentTest.cs
@@ -0,0 +1,36 @@
+﻿using MugenMvvm.Binding.Compiling.Components;
+using MugenMvvm.Binding.Parsing.Expressions;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Compiling
+{
+    public class ConstantExpressionBuilderCompilerComponentTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void TryBuildShouldIgnoreNotConstantExpression()
+        {
+            var component = new ConstantExpressionBuilderCompilerComponent();
+            var ctx = new TestExpressionBuilderContext();
+            component.TryBuild(ctx, MemberExpressionNode.Self).ShouldBeNull();
+        }
+
+        [Fact]
+        public void TryBuildShouldBuildConstantExpression()
+        {
+            var component = new ConstantExpressionBuilderCompilerComponent();
+            var ctx = new TestExpressionBuilderContext();
+            var build = component.TryBuild(ctx, ConstantExpressionNode.False);
+            build.ShouldEqual(ConstantExpressionNode.False.ConstantExpression);
+            build.Invoke().ShouldEqual(false);
+
+            var constantExpressionNode = new ConstantExpressionNode(1);
+            build = component.TryBuild(ctx, constantExpressionNode);
+            build.Invoke().ShouldEqual(constantExpressionNode.Value);
+        }
+
+        #endregion
+    }
+}
