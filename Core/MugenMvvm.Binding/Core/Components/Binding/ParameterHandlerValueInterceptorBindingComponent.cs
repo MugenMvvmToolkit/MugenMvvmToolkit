491c88c1cdf5eea850276d180452e9924bfe1334
Vyacheslav Volkov <vvs0205@outlook.com>
2020-03-04 18:36:29 +0100

    minor changes

--- Core/MugenMvvm.Binding/Core/Components/Binding/ParameterHandlerValueInterceptorBindingComponent.cs
+++ Core/MugenMvvm.Binding/Core/Components/Binding/ParameterHandlerValueInterceptorBindingComponent.cs
@@ -1,5 +1,4 @@
 ﻿using MugenMvvm.Binding.Constants;
-using MugenMvvm.Binding.Converters;
 using MugenMvvm.Binding.Extensions;
 using MugenMvvm.Binding.Interfaces.Converters;
 using MugenMvvm.Binding.Interfaces.Core;
