65043f2d0fa21ad0d7a51e84b263f8e31a36bfcf
Vyacheslav Volkov <vvs0205@outlook.com>
2020-04-07 17:01:20 +0300

    added MemberManagerTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Members/MemberManagerTest.cs
@@ -0,0 +1,57 @@
+﻿using MugenMvvm.Binding.Interfaces.Members;
+using MugenMvvm.Binding.Members;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Components;
+using MugenMvvm.UnitTest.Components;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Members
+{
+    public class MemberManagerTest : ComponentOwnerTestBase<IMemberManager>
+    {
+        #region Methods
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void GetMembersShouldBeHandledByComponents(int count)
+        {
+            var request = "t";
+            var memberManager = new MemberManager();
+            var member = new TestMemberAccessorInfo();
+            var invokeCount = 0;
+            for (var i = 0; i < count; i++)
+            {
+                var isLast = i == count - 1;
+                var component = new TestMemberManagerComponent
+                {
+                    Priority = -i,
+                    TryGetMembers = (o, type, arg3) =>
+                    {
+                        ++invokeCount;
+                        o.ShouldEqual(request);
+                        type.ShouldEqual(request.GetType());
+                        arg3.ShouldEqual(DefaultMetadata);
+                        if (isLast)
+                            return member;
+                        return default;
+                    }
+                };
+                memberManager.AddComponent(component);
+            }
+
+            var result = memberManager.GetMembers(request, DefaultMetadata);
+            result.Count().ShouldEqual(1);
+            result.Item.ShouldEqual(member);
+            invokeCount.ShouldEqual(count);
+        }
+
+        protected override IMemberManager GetComponentOwner(IComponentCollectionProvider? collectionProvider = null)
+        {
+            return new MemberManager(collectionProvider);
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Binding/Members/TestMemberProviderComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MugenMvvm.Binding.Interfaces.Members;
+using MugenMvvm.Binding.Interfaces.Members.Components;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.UnitTest.Binding.Members
+{
+    public class TestMemberProviderComponent : IMemberProviderComponent, IHasPriority
+    {
+        #region Properties
+
+        public Func<Type, string, IReadOnlyMetadataContext?, ItemOrList<IMemberInfo, IReadOnlyList<IMemberInfo>>>? TryGetMembers { get; set; }
+
+        public int Priority { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        ItemOrList<IMemberInfo, IReadOnlyList<IMemberInfo>> IMemberProviderComponent.TryGetMembers(Type type, string name, IReadOnlyMetadataContext? metadata)
+        {
+            return TryGetMembers?.Invoke(type, name, metadata) ?? default;
+        }
+
+        #endregion
+    }
+}
