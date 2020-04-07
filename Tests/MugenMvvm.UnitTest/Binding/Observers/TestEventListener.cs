b15f144bf15c19aef8b254a76e5113d3faa04e79
Vyacheslav Volkov <vvs0205@outlook.com>
2020-04-07 17:12:10 +0300

    added ConstantMemberInfoTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Observers/TestEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MugenMvvm.Binding.Interfaces.Observers;
+
+namespace MugenMvvm.UnitTest.Binding.Observers
+{
+    public class TestEventListener : IEventListener
+    {
+        #region Properties
+
+        public bool IsAlive { get; set; } = true;
+
+        public bool IsWeak { get; set; }
+
+        public Func<object, object?, bool>? TryHandle { get; set; }
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        bool IEventListener.TryHandle(object sender, object? message)
+        {
+            return TryHandle?.Invoke(sender, message) ?? true;
+        }
+
+        #endregion
+    }
+}
