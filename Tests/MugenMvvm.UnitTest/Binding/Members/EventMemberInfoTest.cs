7fc57ef193125fa71efbb83b3f09f689840ef0ad
Vyacheslav Volkov <vvs0205@outlook.com>
2020-04-12 11:19:32 +0300

    added EventMemberInfoTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Members/EventMemberInfoTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MugenMvvm.Binding.Enums;
+using MugenMvvm.Binding.Members;
+using MugenMvvm.Binding.Observers;
+using MugenMvvm.UnitTest.Binding.Observers;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Members
+{
+    public class EventMemberInfoTest : UnitTestBase
+    {
+        #region Events
+
+        public event Action Event1;
+
+        public event Action<object> Event2;
+
+        #endregion
+
+        #region Methods
+
+        [Theory]
+        [InlineData(nameof(Event1))]
+        [InlineData(nameof(Event2))]
+        public void ConstructorShouldInitializeMember(string eventName)
+        {
+            var eventInfo = GetType().GetEvent(eventName);
+            eventInfo.ShouldNotBeNull();
+            var name = eventName + "t";
+            var testEventListener = new TestEventListener();
+            var result = new ActionToken((o, o1) => { });
+            var count = 0;
+
+            var memberObserver = new MemberObserver((target, member, listener, meta) =>
+            {
+                ++count;
+                target.ShouldEqual(this);
+                member.ShouldEqual(eventInfo);
+                listener.ShouldEqual(testEventListener);
+                meta.ShouldEqual(DefaultMetadata);
+                return result;
+            }, eventInfo);
+            var memberInfo = new EventMemberInfo(name, eventInfo, memberObserver);
+            memberInfo.Type.ShouldEqual(eventInfo.EventHandlerType);
+            memberInfo.DeclaringType.ShouldEqual(eventInfo.DeclaringType);
+            memberInfo.UnderlyingMember.ShouldEqual(eventInfo);
+            memberInfo.MemberType.ShouldEqual(MemberType.Event);
+            memberInfo.AccessModifiers.ShouldEqual(MemberFlags.Public | MemberFlags.Instance);
+
+            memberInfo.TrySubscribe(this, testEventListener, DefaultMetadata).ShouldEqual(result);
+            count.ShouldEqual(1);
+        }
+
+        #endregion
+    }
+}
