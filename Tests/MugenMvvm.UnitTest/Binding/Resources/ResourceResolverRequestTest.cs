e53471ca905c4c559816f0c3ed7d9cf833f34282
Vyacheslav Volkov <vvs0205@outlook.com>
2020-04-29 11:04:14 +0300

    added ResourceResolverRequestTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Resources/ResourceResolverRequestTest.cs
@@ -0,0 +1,32 @@
+﻿using MugenMvvm.Binding.Resources;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Resources
+{
+    public class ResourceResolverRequestTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void DefaultShouldBeEmpty()
+        {
+            default(ResourceResolverRequest).IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            var target = typeof(object);
+            var source = "";
+            var state = "test";
+            var memberManagerRequest = new ResourceResolverRequest(target, source, state);
+            memberManagerRequest.Target.ShouldEqual(target);
+            memberManagerRequest.Source.ShouldEqual(source);
+            memberManagerRequest.State.ShouldEqual(state);
+            memberManagerRequest.IsEmpty.ShouldBeFalse();
+        }
+
+        #endregion
+    }
+}
