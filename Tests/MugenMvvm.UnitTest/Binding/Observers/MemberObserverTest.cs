edb725fc768ec6fef425f27b17b2c3c471e2a1df
Vyacheslav Volkov <vvs0205@outlook.com>
2020-04-29 15:56:13 +0300

    added MemberObserverTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Observers/MemberObserverTest.cs
@@ -0,0 +1,43 @@
+﻿using MugenMvvm.Binding.Observers;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Observers
+{
+    public class MemberObserverTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void DefaultShouldBeEmpty()
+        {
+            default(MemberObserver).IsEmpty.ShouldBeTrue();
+            default(MemberObserver).TryObserve(this, new TestEventListener(), DefaultMetadata).IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void TryObserveShouldCallDelegate()
+        {
+            var count = 0;
+            var target = new object();
+            var listener = new TestEventListener();
+            var member = new object();
+            var result = ActionToken.NoDoToken;
+            var observer = new MemberObserver((t, m, l, meta) =>
+            {
+                ++count;
+                t.ShouldEqual(target);
+                m.ShouldEqual(member);
+                l.ShouldEqual(listener);
+                meta.ShouldEqual(DefaultMetadata);
+                return result;
+            }, member);
+
+            observer.Member.ShouldEqual(member);
+            observer.TryObserve(target, listener, DefaultMetadata).ShouldEqual(result);
+            count.ShouldEqual(1);
+        }
+
+        #endregion
+    }
+}
