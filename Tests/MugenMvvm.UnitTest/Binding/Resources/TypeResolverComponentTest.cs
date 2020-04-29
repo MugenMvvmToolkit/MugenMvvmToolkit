7cf3d65bfa2882fde70e74a8cfef3837b57a2298
Vyacheslav Volkov <vvs0205@outlook.com>
2020-04-29 11:41:13 +0300

    added TypeResolverComponentTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Resources/TypeResolverComponentTest.cs
@@ -0,0 +1,40 @@
+﻿using MugenMvvm.Binding.Resources.Components;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Resources
+{
+    public class TypeResolverComponentTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void TryGetResourceValueShouldReturnNullEmpty()
+        {
+            var component = new TypeResolverComponent();
+            component.TryGetType("test", this, DefaultMetadata).ShouldBeNull();
+        }
+
+        [Fact]
+        public void TryGetResourceValueAddRemoveResource()
+        {
+            var resource = typeof(string);
+            var component = new TypeResolverComponent();
+            component.Types.Clear();
+
+            component.AddType(resource);
+            component.Types.Count.ShouldEqual(2);
+            component.Types[resource.Name].ShouldEqual(resource);
+            component.Types[resource.FullName].ShouldEqual(resource);
+            component.TryGetType(resource.Name, this, DefaultMetadata).ShouldEqual(resource);
+            component.TryGetType(resource.FullName, this, DefaultMetadata).ShouldEqual(resource);
+
+            component.Types.Remove(resource.Name);
+            component.Types.Remove(resource.FullName);
+            component.TryGetType(resource.FullName, this, DefaultMetadata).ShouldBeNull();
+            component.TryGetType(resource.Name, this, DefaultMetadata).ShouldBeNull();
+        }
+
+        #endregion
+    }
+}
