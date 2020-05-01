d87f4f81d106f8b5d03635b44cf0582c94303b0b
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-01 14:41:16 +0300

    added PropertyChangedMemberObserverProviderComponentTest minor changes

--- Tests/MugenMvvm.UnitTest/Models/TestNotifyPropertyChangedModel.cs
+++ Tests/MugenMvvm.UnitTest/Models/TestNotifyPropertyChangedModel.cs
@@ -5,6 +5,28 @@ namespace MugenMvvm.UnitTest.Models
 {
     public class TestNotifyPropertyChangedModel : INotifyPropertyChanged
     {
+        #region Fields
+
+        private string? _property;
+
+        #endregion
+
+        #region Properties
+
+        public string? Property
+        {
+            get => _property;
+            set
+            {
+                if (_property == value)
+                    return;
+                _property = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
         #region Events
 
         public event PropertyChangedEventHandler? PropertyChanged;
