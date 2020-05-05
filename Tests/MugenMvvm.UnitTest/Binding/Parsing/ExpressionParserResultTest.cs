88545070737aec5802037b8a6b5d6dbd4c20babb
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-05 17:56:13 +0300

    added ExpressionParserResultTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Parsing/ExpressionParserResultTest.cs
@@ -0,0 +1,50 @@
+﻿using MugenMvvm.Binding.Parsing;
+using MugenMvvm.Binding.Parsing.Expressions;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.UnitTest.Metadata;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Parsing
+{
+    public class ExpressionParserResultTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void DefaultShouldBeEmpty()
+        {
+            default(ExpressionParserResult).IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues1()
+        {
+            var target = MemberExpressionNode.Source;
+            var source = MemberExpressionNode.Args;
+            var parameter = new[] {MemberExpressionNode.Self, MemberExpressionNode.Binding};
+            var memberManagerRequest = new ExpressionParserResult(target, source, parameter, DefaultMetadata);
+            memberManagerRequest.Target.ShouldEqual(target);
+            memberManagerRequest.Source.ShouldEqual(source);
+            memberManagerRequest.IsEmpty.ShouldBeFalse();
+            memberManagerRequest.Parameters.ShouldEqual(parameter);
+            memberManagerRequest.Metadata.ShouldEqual(DefaultMetadata);
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues2()
+        {
+            var target = MemberExpressionNode.Source;
+            var source = MemberExpressionNode.Args;
+            var parameter = new[] {MemberExpressionNode.Self, MemberExpressionNode.Binding};
+            var memberManagerRequest = new ExpressionParserResult(target, source, parameter, new TestMetadataOwner<IReadOnlyMetadataContext> {Metadata = DefaultMetadata, HasMetadata = true});
+            memberManagerRequest.Target.ShouldEqual(target);
+            memberManagerRequest.Source.ShouldEqual(source);
+            memberManagerRequest.IsEmpty.ShouldBeFalse();
+            memberManagerRequest.Parameters.ShouldEqual(parameter);
+            memberManagerRequest.Metadata.ShouldEqual(DefaultMetadata);
+        }
+
+        #endregion
+    }
+}
