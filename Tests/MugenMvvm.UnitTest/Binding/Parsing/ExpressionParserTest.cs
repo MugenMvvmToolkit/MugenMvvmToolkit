9159ce2b5e61a364481f529c1ebbbc3bd98f1e73
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-05 17:53:29 +0300

    updated ExpressionParserTest

--- Tests/MugenMvvm.UnitTest/Binding/Parsing/ExpressionParserTest.cs
+++ Tests/MugenMvvm.UnitTest/Binding/Parsing/ExpressionParserTest.cs
@@ -1,15 +1,59 @@
-﻿using MugenMvvm.Binding.Interfaces.Parsing;
+﻿using System;
+using MugenMvvm.Binding.Interfaces.Parsing;
 using MugenMvvm.Binding.Parsing;
 using MugenMvvm.Binding.Parsing.Components;
+using MugenMvvm.Binding.Parsing.Expressions;
 using MugenMvvm.Extensions;
 using MugenMvvm.Interfaces.Components;
 using MugenMvvm.UnitTest.Components;
+using Should;
+using Xunit;
 
 namespace MugenMvvm.UnitTest.Binding.Parsing
 {
     public class ExpressionParserTest : ComponentOwnerTestBase<ExpressionParser>
     {
         #region Methods
+
+        [Fact]
+        public void ParseMemberPathShouldThrowEmpty()
+        {
+            var parser = new ExpressionParser();
+            ShouldThrow<InvalidOperationException>(() => parser.Parse(this, DefaultMetadata));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void ParseShouldBeHandledByComponents(int componentCount)
+        {
+            var parser = new ExpressionParser();
+            var request = this;
+            var result = new ExpressionParserResult(MemberExpressionNode.Source, ConstantExpressionNode.EmptyString, default);
+            var invokeCount = 0;
+            for (var i = 0; i < componentCount; i++)
+            {
+                var isLast = i == componentCount - 1;
+                var component = new TestExpressionParserComponent
+                {
+                    Priority = -i,
+                    TryParse = (o, arg3, arg4) =>
+                    {
+                        ++invokeCount;
+                        o.ShouldEqual(request);
+                        arg3.ShouldEqual(request.GetType());
+                        arg4.ShouldEqual(DefaultMetadata);
+                        if (isLast)
+                            return result;
+                        return null;
+                    }
+                };
+                parser.AddComponent(component);
+            }
+
+            parser.Parse(request, DefaultMetadata).ShouldEqual(result);
+            invokeCount.ShouldEqual(componentCount);
+        }
 
         protected override ExpressionParser GetComponentOwner(IComponentCollectionProvider? collectionProvider = null)
         {
