26ae74ad358b876ff8f861c8d0ffc26297897467
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-05 15:59:31 +0300

    added ExpressionConverterRequestTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Parsing/ExpressionConverterRequestTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using MugenMvvm.Binding.Parsing;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Parsing
+{
+    public class ExpressionConverterRequestTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void DefaultShouldBeEmpty()
+        {
+            default(ExpressionConverterRequest).IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            var target = Expression.Constant("1");
+            var source = Expression.Constant("2");
+            var parameter = new KeyValuePair<string, object>("", "");
+            var memberManagerRequest = new ExpressionConverterRequest(target, source, parameter);
+            memberManagerRequest.Target.ShouldEqual(target);
+            memberManagerRequest.Source.ShouldEqual(source);
+            memberManagerRequest.IsEmpty.ShouldBeFalse();
+            memberManagerRequest.Parameters.ShouldEqual(parameter);
+        }
+
+        #endregion
+    }
+}
