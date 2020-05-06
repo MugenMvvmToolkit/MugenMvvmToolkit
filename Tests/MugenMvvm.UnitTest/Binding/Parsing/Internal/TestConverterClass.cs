96141dfceab3314a4b92783428148afa9d484635
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-06 18:56:16 +0300

    added  IndexerExpressionConverterTest minor changes

---
+++ Tests/MugenMvvm.UnitTest/Binding/Parsing/Internal/TestConverterClass.cs
@@ -0,0 +1,35 @@
+﻿namespace MugenMvvm.UnitTest.Binding.Parsing.Internal
+{
+    public class TestConverterClass
+    {
+        #region Fields
+
+        public object? Field;
+
+        #endregion
+
+        #region Properties
+
+        public object? this[object index] => null;
+
+        public object? Property { get; set; }
+
+        public object? PropertyStatic { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public object? Method()
+        {
+            return null;
+        }
+
+        public object? MethodStatic()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Binding/Parsing/Internal/TestResourceExtensionClass.cs
@@ -0,0 +1,52 @@
+﻿using MugenMvvm.Binding.Attributes;
+
+namespace MugenMvvm.UnitTest.Binding.Parsing.Internal
+{
+    [ResourceBindingSyntaxExtension(ClassResource)]
+    public class TestResourceExtensionClass
+    {
+        #region Fields
+
+        public object? Field;
+
+        [ResourceBindingSyntaxExtension(FieldResource)]
+        public object? FieldResourceExt;
+
+        public const string ClassResource = "c1";
+        public const string MethodResource = "c2";
+        public const string FieldResource = "c3";
+        public const string PropertyResource = "c4";
+        public const string IndexerResource = "c5";
+
+        #endregion
+
+        #region Properties
+
+        [ResourceBindingSyntaxExtension(IndexerResource)]
+        public object? this[int index] => null;
+
+        public object? this[object index] => null;
+
+        public object? Property { get; set; }
+
+        [ResourceBindingSyntaxExtension(PropertyResource)]
+        public object? PropertyResourceExt { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        [ResourceBindingSyntaxExtension(MethodResource)]
+        public object? MethodResourceExt()
+        {
+            return null;
+        }
+
+        public object? Method()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
