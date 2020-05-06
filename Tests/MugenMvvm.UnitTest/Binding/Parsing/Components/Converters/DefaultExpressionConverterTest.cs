a6ac358273ae7efc72af800dafce0753ebb5ebfb
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-06 14:48:05 +0300

    added DefaultExpressionConverterTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Parsing/Components/Converters/DefaultExpressionConverterTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using MugenMvvm.Binding.Parsing;
+using MugenMvvm.Binding.Parsing.Components.Converters;
+using MugenMvvm.Binding.Parsing.Expressions;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Parsing.Components.Converters
+{
+    public class DefaultExpressionConverterTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void TryConvertShouldIgnoreNotDefaultExpression()
+        {
+            var component = new DefaultExpressionConverter();
+            var ctx = new ExpressionConverterContext<Expression>();
+            component.TryConvert(ctx, Expression.Parameter(typeof(object))).ShouldBeNull();
+        }
+
+        [Fact]
+        public void TryConvertShouldConvertConstantExpression()
+        {
+            var component = new DefaultExpressionConverter();
+            var ctx = new ExpressionConverterContext<Expression>();
+            component.TryConvert(ctx, Expression.Default(typeof(int))).EqualsEx(ConstantExpressionNode.Get(0)).ShouldBeTrue();
+        }
+
+        #endregion
+    }
+}
