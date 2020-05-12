55ee32cd5bc2ea3b8904db87d8472bdf9e1bb926
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-12 14:08:47 +0300

    added MultiBindingTest

---
+++ Tests/MugenMvvm.UnitTest/Binding/Core/MultiBindingTest.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using MugenMvvm.Binding.Compiling;
+using MugenMvvm.Binding.Core;
+using MugenMvvm.Binding.Enums;
+using MugenMvvm.Binding.Interfaces.Core;
+using MugenMvvm.Binding.Interfaces.Observers;
+using MugenMvvm.Binding.Observers;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Components;
+using MugenMvvm.UnitTest.Binding.Compiling.Internal;
+using MugenMvvm.UnitTest.Binding.Core.Internal;
+using MugenMvvm.UnitTest.Binding.Members.Internal;
+using MugenMvvm.UnitTest.Binding.Observers.Internal;
+using MugenMvvm.UnitTest.Internal.Internal;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Core
+{
+    public class MultiBindingTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public virtual void DisposeShouldDisposeExpression()
+        {
+            var targetDisposed = false;
+            var sourceDisposed = false;
+            var expressionDisposed = false;
+            IMemberPathObserverListener? targetListener = null;
+            IMemberPathObserverListener? sourceListener = null;
+            var expression = new TestCompiledExpression {Dispose = () => expressionDisposed = true};
+            var target = new TestMemberPathObserver
+            {
+                Dispose = () => targetDisposed = true,
+                AddListener = l =>
+                {
+                    targetListener.ShouldBeNull();
+                    targetListener = l;
+                },
+                RemoveListener = l =>
+                {
+                    targetListener.ShouldEqual(l);
+                    targetListener = null;
+                }
+            };
+            var source = new TestMemberPathObserver
+            {
+                Dispose = () => sourceDisposed = true,
+                AddListener = l =>
+                {
+                    sourceListener.ShouldBeNull();
+                    sourceListener = l;
+                },
+                RemoveListener = l =>
+                {
+                    sourceListener.ShouldEqual(l);
+                    sourceListener = null;
+                }
+            };
+
+            var components = new IComponent<IBinding>[] {new TestBindingTargetObserverListener(), new TestBindingSourceObserverListener()};
+            var binding = new MultiBinding(target, sources: source, expression: expression);
+            binding.State.ShouldEqual(BindingState.Valid);
+            binding.Initialize(components, DefaultMetadata);
+            targetListener.ShouldEqual(binding);
+            sourceListener.ShouldEqual(binding);
+
+            var disposeCount = 0;
+            var testLifecycleListener = new TestBindingStateDispatcherComponent
+            {
+                OnLifecycleChanged = (b, state, _, __, m) =>
+                {
+                    ++disposeCount;
+                    b.ShouldEqual(binding);
+                    state.ShouldEqual(BindingLifecycleState.Disposed);
+                    m.ShouldBeNull();
+                    return null;
+                }
+            };
+            using var subscribe = TestComponentSubscriber.Subscribe(testLifecycleListener);
+
+            binding.Dispose();
+            binding.State.ShouldEqual(BindingState.Disposed);
+            targetDisposed.ShouldBeTrue();
+            sourceDisposed.ShouldBeTrue();
+            binding.GetComponents().ToArray().ShouldBeEmpty();
+            targetListener.ShouldBeNull();
+            sourceListener.ShouldBeNull();
+            disposeCount.ShouldEqual(1);
+            expressionDisposed.ShouldBeTrue();
+
+            binding.AddComponent(components[0]).ShouldBeFalse();
+            binding.GetComponents().ToArray().ShouldBeEmpty();
+            ShouldThrow<ObjectDisposedException>(() =>
+            {
+                var compiledExpression = binding.Expression;
+            });
+        }
+
+        [Fact]
+        public void UpdateTargetShouldUseObserverValue()
+        {
+            MultiBinding? binding = null;
+            var targetObj = new object();
+            var sourceObj = new object();
+            object sourceValue = "2";
+            object expressionValue = "3";
+            int targetSet = 0, sourceGet = 0, expressionInvoke = 0;
+
+            var expression = new TestCompiledExpression
+            {
+                Invoke = (list, context) =>
+                {
+                    ++expressionInvoke;
+                    list.ToArray().SequenceEqual(new[] {new ExpressionValue(sourceValue.GetType(), sourceValue)}).ShouldBeTrue();
+                    context.ShouldEqual(binding);
+                    return expressionValue;
+                }
+            };
+            var targetMember = new TestMemberAccessorInfo
+            {
+                Type = typeof(object),
+                GetValue = (o, context) => throw new NotSupportedException()
+            };
+            targetMember.SetValue = (o, v, context) =>
+            {
+                ++targetSet;
+                o.ShouldEqual(targetObj);
+                v.ShouldEqual(targetMember.MemberType == MemberType.Event ? binding : expressionValue);
+                context.ShouldEqual(binding);
+            };
+            var sourceMember = new TestMemberAccessorInfo
+            {
+                Type = typeof(object),
+                GetValue = (o, context) =>
+                {
+                    ++sourceGet;
+                    o.ShouldEqual(sourceObj);
+                    context.ShouldEqual(binding);
+                    return sourceValue;
+                },
+                SetValue = (o, v, context) => throw new NotSupportedException()
+            };
+            var target = new TestMemberPathObserver
+            {
+                GetLastMember = metadata => new MemberPathLastMember(targetObj, targetMember)
+            };
+            var source = new TestMemberPathObserver
+            {
+                GetLastMember = metadata => new MemberPathLastMember(sourceObj, sourceMember)
+            };
+
+            binding = new MultiBinding(target, sources: source, expression: expression);
+            binding.Expression.ShouldEqual(expression);
+            binding.UpdateTarget();
+            targetSet.ShouldEqual(1);
+            sourceGet.ShouldEqual(1);
+            expressionInvoke.ShouldEqual(1);
+
+            targetMember.MemberType = MemberType.Event;
+            binding.UpdateTarget();
+            targetSet.ShouldEqual(2);
+            sourceGet.ShouldEqual(1);
+            expressionInvoke.ShouldEqual(1);
+
+            binding.Invoke().ShouldEqual(expressionValue);
+            sourceGet.ShouldEqual(2);
+            expressionInvoke.ShouldEqual(2);
+        }
+
+        #endregion
+    }
+}
