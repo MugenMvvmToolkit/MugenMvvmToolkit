2c3b3a46f1a3fd8b333e89ca74b1701b77ab9725
Vyacheslav Volkov <vvs0205@outlook.com>
2020-05-30 19:34:22 +0300

    minor changes

---
+++ Core/MugenMvvm.Core/Requests/ViewModelViewRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using MugenMvvm.Interfaces.ViewModels;
+
+namespace MugenMvvm.Requests
+{
+    [StructLayout(LayoutKind.Auto)]
+    public readonly struct ViewModelViewRequest
+    {
+        #region Fields
+
+        public readonly object? View;
+        public readonly IViewModelBase? ViewModel;
+
+        #endregion
+
+        #region Constructors
+
+        public ViewModelViewRequest(IViewModelBase? viewModel, object? view)
+        {
+            ViewModel = viewModel;
+            View = view;
+        }
+
+        #endregion
+    }
+}
