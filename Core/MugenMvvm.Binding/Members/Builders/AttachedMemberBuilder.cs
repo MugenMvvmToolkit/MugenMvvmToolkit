66c81d032dbd1e791f2b149e5af423d8d18f75bb
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-11 12:08:28 +0300

    added AttachedMemberBuilder added AttachedMemberBuilderTest minor changes

---
+++ Core/MugenMvvm.Binding/Members/Builders/AttachedMemberBuilder.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using MugenMvvm.Binding.Delegates;
+using MugenMvvm.Binding.Enums;
+using MugenMvvm.Binding.Interfaces.Members;
+using MugenMvvm.Binding.Interfaces.Observers;
+using MugenMvvm.Binding.Members.Descriptors;
+using MugenMvvm.Binding.Observers;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.Binding.Members.Builders
+{
+    public static class AttachedMemberBuilder
+    {
+        #region Fields
+
+        private static readonly Dictionary<string, EventListenerCollection> StaticEvents = new Dictionary<string, EventListenerCollection>();
+        private static readonly Dictionary<string, object?> StaticValues = new Dictionary<string, object?>();
+
+        #endregion
+
+        #region Methods
+
+        public static EventBuilder<TTarget> Event<TTarget>(BindableEventDescriptor<TTarget> descriptor, Type? eventType = null) where TTarget : class
+        {
+            return Event<TTarget>(descriptor.Name, null, eventType);
+        }
+
+        public static EventBuilder<TTarget> Event<TTarget>(string name, Type? declaringType = null, Type? eventType = null) where TTarget : class?
+        {
+            return new EventBuilder<TTarget>(name, declaringType ?? typeof(TTarget), eventType ?? typeof(EventHandler));
+        }
+
+        public static PropertyBuilder<TTarget, TValue> Property<TTarget, TValue>(BindablePropertyDescriptor<TTarget, TValue> descriptor)
+            where TTarget : class
+        {
+            return Property<TTarget, TValue>(descriptor.Name);
+        }
+
+        public static PropertyBuilder<TTarget, TValue> Property<TTarget, TValue>(string name, Type? declaringType = null, Type? propertyType = null)
+            where TTarget : class?
+        {
+            return new PropertyBuilder<TTarget, TValue>(name, declaringType ?? typeof(TTarget), propertyType ?? typeof(TValue));
+        }
+
+        public static MethodBuilder<TTarget, TReturn> Method<TTarget, TReturn>(BindableMethodDescriptor<TTarget, TReturn> descriptor) where TTarget : class
+        {
+            return Method<TTarget, TReturn>(descriptor.Name, typeof(TTarget), typeof(TReturn));
+        }
+
+        public static MethodBuilder<TTarget, TReturn> Method<TTarget, TReturn>(string name, Type? declaringType = null, Type? returnType = null) where TTarget : class?
+        {
+            return new MethodBuilder<TTarget, TReturn>(name, declaringType ?? typeof(TTarget), returnType ?? typeof(TReturn));
+        }
+
+        public static ParameterBuilder Parameter<TType>(string? name = null)
+        {
+            return new ParameterBuilder(name ?? "", typeof(TType));
+        }
+
+        public static ParameterBuilder Parameter(string name, Type type)
+        {
+            return new ParameterBuilder(name, type);
+        }
+
+        internal static void RaiseMemberAttached<TTarget, TMember>(string id, TTarget target, TMember member, MemberAttachedDelegate<TMember, TTarget> handler, IReadOnlyMetadataContext? metadata)
+            where TTarget : class?
+            where TMember : class, IMemberInfo
+        {
+            Should.NotBeNull(target, nameof(target));
+            Should.NotBeNull(member, nameof(member));
+            Should.NotBeNull(handler, nameof(handler));
+            var attachedValueProvider = MugenService.AttachedValueProvider;
+            if (!attachedValueProvider.Contains(target!, id))
+            {
+#pragma warning disable 8634
+                attachedValueProvider.GetOrAdd(target, id, (member, handler, metadata), (t, state) =>
+                {
+                    state.handler(state.member, t, state.metadata);
+                    return (object?) null;
+                });
+#pragma warning restore 8634
+            }
+        }
+
+        internal static MemberFlags GetFlags(bool isStatic)
+        {
+            return isStatic ? MemberFlags.StaticPublic | MemberFlags.Attached : MemberFlags.InstancePublic | MemberFlags.Attached;
+        }
+
+        internal static string GenerateMemberId(string prefix, Type declaringType, string name)
+        {
+            return prefix + declaringType.FullName.Length + declaringType.Name + declaringType.AssemblyQualifiedName.Length + name;
+        }
+
+        internal static bool TryGetStaticValue<TValue>(string name, out TValue value)
+        {
+            lock (StaticValues)
+            {
+                if (StaticValues.TryGetValue(name, out var valueRaw))
+                {
+                    value = (TValue) valueRaw!;
+                    return true;
+                }
+
+                value = default!;
+                return false;
+            }
+        }
+
+        internal static bool TrySetStaticValue<TValue>(string name, TValue value, out TValue oldValue)
+        {
+            lock (StaticValues)
+            {
+                if (StaticValues.TryGetValue(name, out var valueRaw))
+                {
+                    oldValue = (TValue) valueRaw!;
+                    if (EqualityComparer<TValue>.Default.Equals(oldValue, value))
+                        return false;
+                }
+                else
+                    oldValue = default!;
+
+                StaticValues[name] = BoxingExtensions.Box(value);
+                return true;
+            }
+        }
+
+        internal static ActionToken AddStaticEvent(string name, IEventListener listener)
+        {
+            EventListenerCollection events;
+            lock (StaticEvents)
+            {
+                if (!StaticEvents.TryGetValue(name, out events))
+                {
+                    events = new EventListenerCollection();
+                    StaticEvents[name] = events;
+                }
+            }
+
+            return events.Add(listener);
+        }
+
+        internal static void RaiseStaticEvent<T>(string name, in T message, IReadOnlyMetadataContext? metadata)
+        {
+            lock (StaticEvents)
+            {
+                if (StaticEvents.TryGetValue(name, out var listener))
+                    listener.Raise(null, message, metadata);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Binding/Members/Builders/AttachedMemberBuilderTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using MugenMvvm.Binding.Enums;
+using MugenMvvm.Binding.Members.Builders;
+using MugenMvvm.Binding.Members.Descriptors;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Members.Builders
+{
+    public class AttachedMemberBuilderTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void EventShouldBuildEvent1()
+        {
+            string name = "t";
+            Type declaredType = GetType();
+            var memberType = typeof(Action);
+            var build = AttachedMemberBuilder.Event<object>(name, declaredType, memberType).Build();
+            build.MemberType.ShouldEqual(MemberType.Event);
+            build.Name.ShouldEqual(name);
+            build.DeclaringType.ShouldEqual(declaredType);
+            build.Type.ShouldEqual(memberType);
+        }
+
+        [Fact]
+        public void EventShouldBuildEvent2()
+        {
+            string name = "t";
+            Type declaredType = GetType();
+            var memberType = typeof(Action);
+            BindableEventDescriptor<AttachedMemberBuilderTest> descriptor = name;
+            var build = AttachedMemberBuilder.Event(descriptor, memberType).Build();
+            build.MemberType.ShouldEqual(MemberType.Event);
+            build.Name.ShouldEqual(name);
+            build.DeclaringType.ShouldEqual(declaredType);
+            build.Type.ShouldEqual(memberType);
+        }
+
+        [Fact]
+        public void PropertyShouldBuildProperty1()
+        {
+            string name = "t";
+            Type declaredType = GetType();
+            var memberType = typeof(Action);
+            var build = AttachedMemberBuilder.Property<object, object>(name, declaredType, memberType).Build();
+            build.MemberType.ShouldEqual(MemberType.Accessor);
+            build.Name.ShouldEqual(name);
+            build.DeclaringType.ShouldEqual(declaredType);
+            build.Type.ShouldEqual(memberType);
+        }
+
+        [Fact]
+        public void PropertyShouldBuildProperty2()
+        {
+            string name = "t";
+            Type declaredType = GetType();
+            var memberType = typeof(Action);
+            BindablePropertyDescriptor<AttachedMemberBuilderTest, Action> descriptor = name;
+            var build = AttachedMemberBuilder.Property(descriptor).Build();
+            build.MemberType.ShouldEqual(MemberType.Accessor);
+            build.Name.ShouldEqual(name);
+            build.DeclaringType.ShouldEqual(declaredType);
+            build.Type.ShouldEqual(memberType);
+        }
+
+        [Fact]
+        public void MethodShouldBuildMethod1()
+        {
+            string name = "t";
+            Type declaredType = GetType();
+            var memberType = typeof(Action);
+            var build = AttachedMemberBuilder.Method<object, object>(name, declaredType, memberType).InvokeHandler((member, target, args, metadata) => null!).Build();
+            build.MemberType.ShouldEqual(MemberType.Method);
+            build.Name.ShouldEqual(name);
+            build.DeclaringType.ShouldEqual(declaredType);
+            build.Type.ShouldEqual(memberType);
+        }
+
+        [Fact]
+        public void MethodShouldBuildMethod2()
+        {
+            string name = "t";
+            Type declaredType = GetType();
+            var memberType = typeof(Action);
+            BindableMethodDescriptor<AttachedMemberBuilderTest, Action> descriptor = name;
+            var build = AttachedMemberBuilder.Method(descriptor).InvokeHandler((member, target, args, metadata) => null!).Build();
+            build.MemberType.ShouldEqual(MemberType.Method);
+            build.Name.ShouldEqual(name);
+            build.DeclaringType.ShouldEqual(declaredType);
+            build.Type.ShouldEqual(memberType);
+        }
+
+        [Fact]
+        public void ParameterShouldBuildParameter1()
+        {
+            string name = "t";
+            var type = GetType();
+            var parameterInfo = AttachedMemberBuilder.Parameter(name, type).Build();
+            parameterInfo.ParameterType.ShouldEqual(type);
+            parameterInfo.Name.ShouldEqual(name);
+        }
+        
+        [Fact]
+        public void ParameterShouldBuildParameter2()
+        {
+            string name = "t";
+            var type = GetType();
+            var parameterInfo = AttachedMemberBuilder.Parameter<AttachedMemberBuilderTest>(name).Build();
+            parameterInfo.ParameterType.ShouldEqual(type);
+            parameterInfo.Name.ShouldEqual(name);
+        }
+
+        #endregion
+    }
+}
