f266b922e6630db4ea9825f31ce5485311e6923a
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-14 12:34:23 +0300

    added TypeViewModelProvider minor changes

---
+++ Core/MugenMvvm.Core/ViewModels/Components/TypeViewModelProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MugenMvvm.Attributes;
+using MugenMvvm.Components;
+using MugenMvvm.Constants;
+using MugenMvvm.Enums;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.ViewModels;
+using MugenMvvm.Interfaces.ViewModels.Components;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.ViewModels.Components
+{
+    public sealed class TypeViewModelProvider : AttachableComponentBase<IViewModelManager>, IViewModelProviderComponent, IHasPriority
+    {
+        #region Fields
+
+        private readonly IServiceProvider? _serviceProvider;
+
+        #endregion
+
+        #region Constructors
+
+        [Preserve(Conditional = true)]
+        public TypeViewModelProvider(IServiceProvider? serviceProvider = null)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Priority { get; set; } = ViewModelComponentPriority.Provider;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public IViewModelBase? TryGetViewModel<TRequest>(in TRequest request, IReadOnlyMetadataContext? metadata)
+        {
+            if (Default.IsValueType<TRequest>() || !(request is Type type))
+                return null;
+
+            var viewModel = (IViewModelBase) _serviceProvider.DefaultIfNull().GetService(type);
+            if (viewModel != null)
+            {
+                Owner.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Initializing, request, metadata);
+                Owner.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Initialized, request, metadata);
+            }
+
+            return viewModel;
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/ViewModels/Components/TypeViewModelProviderTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using MugenMvvm.Enums;
+using MugenMvvm.Extensions;
+using MugenMvvm.UnitTest.Internal.Internal;
+using MugenMvvm.UnitTest.ViewModels.Internal;
+using MugenMvvm.ViewModels;
+using MugenMvvm.ViewModels.Components;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.ViewModels.Components
+{
+    public class TypeViewModelProviderTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ShouldIgnoreNonGuidRequest()
+        {
+            var component = new TypeViewModelProvider();
+            component.TryGetViewModel(this, DefaultMetadata).ShouldBeNull();
+        }
+
+        [Fact]
+        public void ShouldUseServiceResolverAndNotifyLifecycle()
+        {
+            var viewModel = new TestViewModel();
+            var lifeCycles = new List<ViewModelLifecycleState>();
+            var manager = new ViewModelManager();
+            manager.AddComponent(new TestViewModelLifecycleDispatcherComponent
+            {
+                OnLifecycleChanged = (vm, state, arg3, arg4, m) =>
+                {
+                    vm.ShouldEqual(viewModel);
+                    m.ShouldEqual(DefaultMetadata);
+                    lifeCycles.Add(state);
+                }
+            });
+            manager.AddComponent(new TypeViewModelProvider(new TestServiceProvider
+            {
+                GetService = type =>
+                {
+                    type.ShouldEqual(viewModel.GetType());
+                    return viewModel;
+                }
+            }));
+
+            manager.TryGetViewModel(viewModel.GetType(), DefaultMetadata).ShouldEqual(viewModel);
+            lifeCycles.Count.ShouldEqual(2);
+            lifeCycles[0].ShouldEqual(ViewModelLifecycleState.Initializing);
+            lifeCycles[1].ShouldEqual(ViewModelLifecycleState.Initialized);
+        }
+
+        #endregion
+    }
+}
