22212284a4f91de8b51f2847da63d7ab637ae1cd
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-14 13:44:54 +0300

    added RawViewLifecycleDispatcher minor changes

---
+++ Core/MugenMvvm.Core/Views/Components/RawViewLifecycleDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using MugenMvvm.Components;
+using MugenMvvm.Constants;
+using MugenMvvm.Enums;
+using MugenMvvm.Extensions.Internal;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.Views;
+using MugenMvvm.Interfaces.Views.Components;
+
+namespace MugenMvvm.Views.Components
+{
+    public sealed class RawViewLifecycleDispatcher : AttachableComponentBase<IViewManager>, IViewLifecycleDispatcherComponent, IHasPriority
+    {
+        #region Properties
+
+        public int Priority { get; set; } = ComponentPriority.PreInitializer;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public void OnLifecycleChanged<TState>(object view, ViewLifecycleState lifecycleState, in TState state, IReadOnlyMetadataContext? metadata)
+        {
+            if (view is IView)
+                return;
+
+            var list = Owner.GetViews(view, metadata);
+            for (var i = 0; i < list.Count(); i++)
+                Owner.OnLifecycleChanged(list.Get(i), lifecycleState, state, metadata);
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Views/Components/RawViewLifecycleDispatcherTest.cs
@@ -0,0 +1,61 @@
+﻿using MugenMvvm.Enums;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.ViewModels;
+using MugenMvvm.UnitTest.ViewModels.Internal;
+using MugenMvvm.UnitTest.Views.Internal;
+using MugenMvvm.Views;
+using MugenMvvm.Views.Components;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Views.Components
+{
+    public class RawViewLifecycleDispatcherTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ShouldResendEvent()
+        {
+            var state = "test";
+            var st = ViewLifecycleState.Initialized;
+            var viewModel = new TestViewModel();
+            var view = new View(new ViewModelViewMapping("1", typeof(string), typeof(IViewModelBase)), this, viewModel);
+            var invokeCount = 0;
+
+            var viewManager = new ViewManager();
+            viewManager.AddComponent(new RawViewLifecycleDispatcher());
+            viewManager.AddComponent(new TestViewProviderComponent
+            {
+                TryGetViews = (o, type, arg3) =>
+                {
+                    o.ShouldEqual(this);
+                    arg3.ShouldEqual(DefaultMetadata);
+                    return view;
+                }
+            });
+            viewManager.AddComponent(new TestViewLifecycleDispatcherComponent
+            {
+                OnLifecycleChanged = (o, lifecycleState, arg3, arg4, arg5) =>
+                {
+                    if (o == this)
+                        return;
+                    ++invokeCount;
+                    o.ShouldEqual(view);
+                    lifecycleState.ShouldEqual(st);
+                    arg3.ShouldEqual(state);
+                    arg5.ShouldEqual(DefaultMetadata);
+                }
+            });
+
+            viewManager.OnLifecycleChanged(this, st, state, DefaultMetadata);
+            invokeCount.ShouldEqual(1);
+
+            invokeCount = 0;
+            viewManager.OnLifecycleChanged(view, st, state, DefaultMetadata);
+            invokeCount.ShouldEqual(1);
+        }
+
+        #endregion
+    }
+}
