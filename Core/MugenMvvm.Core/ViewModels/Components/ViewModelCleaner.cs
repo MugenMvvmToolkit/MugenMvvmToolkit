b7e7f3a6262cc4234e0b3a9379cdebf0c0d2bb3c
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-14 13:36:37 +0300

    added ViewModelCleaner minor changes

---
+++ Core/MugenMvvm.Core/ViewModels/Components/ViewModelCleaner.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Input;
+using MugenMvvm.Collections.Internal;
+using MugenMvvm.Constants;
+using MugenMvvm.Enums;
+using MugenMvvm.Extensions;
+using MugenMvvm.Extensions.Internal;
+using MugenMvvm.Interfaces.Busy;
+using MugenMvvm.Interfaces.Internal;
+using MugenMvvm.Interfaces.Messaging;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.ViewModels;
+using MugenMvvm.Interfaces.ViewModels.Components;
+using MugenMvvm.Interfaces.Views;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.ViewModels.Components
+{
+    public class ViewModelCleaner : IViewModelLifecycleDispatcherComponent, IHasPriority
+    {
+        #region Fields
+
+        private readonly IAttachedValueProvider? _attachedValueProvider;
+        private readonly IReflectionDelegateProvider? _reflectionDelegateProvider;
+        private readonly IViewManager? _viewManager;
+
+        private static readonly TypeLightDictionary<object?> TypesToCommandsProperties = new TypeLightDictionary<object?>(59);
+
+        #endregion
+
+        #region Constructors
+
+        public ViewModelCleaner(IViewManager? viewManager = null, IAttachedValueProvider? attachedValueProvider = null, IReflectionDelegateProvider? reflectionDelegateProvider = null)
+        {
+            _viewManager = viewManager;
+            _attachedValueProvider = attachedValueProvider;
+            _reflectionDelegateProvider = reflectionDelegateProvider;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Priority { get; set; } = ComponentPriority.PostInitializer;
+
+        public bool CleanupCommands { get; set; } = true;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public void OnLifecycleChanged<TState>(IViewModelBase viewModel, ViewModelLifecycleState lifecycleState, in TState state, IReadOnlyMetadataContext? metadata)
+        {
+            if (lifecycleState == ViewModelLifecycleState.Disposed)
+                Cleanup(viewModel, lifecycleState, state, metadata);
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected virtual void Cleanup<TState>(IViewModelBase viewModel, ViewModelLifecycleState lifecycleState, in TState state, IReadOnlyMetadataContext? metadata)
+        {
+            if (CleanupCommands)
+                DisposeCommands(viewModel);
+            var viewManager = _viewManager.DefaultIfNull();
+            var views = viewManager.GetViews(viewModel, metadata);
+            for (var i = 0; i < views.Count(); i++)
+                viewManager.CleanupAsync(views.Get(i), state, default, metadata);
+
+            var busyManager = viewModel.TryGetOptionalService<IBusyManager>();
+            if (busyManager != null)
+            {
+                busyManager.ClearBusy();
+                busyManager.ClearComponents(metadata);
+            }
+
+            var messenger = viewModel.TryGetOptionalService<IMessenger>();
+            if (messenger != null)
+            {
+                messenger.UnsubscribeAll(metadata);
+                messenger.ClearComponents(metadata);
+            }
+
+            viewModel.ClearMetadata(true);
+            _attachedValueProvider.DefaultIfNull().Clear(viewModel);
+            (viewModel as IValueHolder<IWeakReference>)?.ReleaseWeakReference();
+        }
+
+        protected void DisposeCommands(IViewModelBase viewModel)
+        {
+            object? rawValue;
+            Type type = viewModel.GetType();
+            lock (TypesToCommandsProperties)
+            {
+                if (!TypesToCommandsProperties.TryGetValue(type, out rawValue))
+                {
+                    ItemOrList<Func<object, ICommand>, List<Func<object, ICommand>>> items = default;
+                    foreach (var p in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        if (typeof(ICommand).IsAssignableFrom(p.PropertyType) && p.CanRead &&
+                            p.GetIndexParameters().Length == 0)
+                        {
+                            var func = p.GetMemberGetter<object, ICommand>(_reflectionDelegateProvider);
+                            items.Add(func);
+                        }
+                    }
+
+                    rawValue = items.GetRawValue();
+                    TypesToCommandsProperties[type] = rawValue;
+                }
+            }
+
+            if (rawValue == null)
+                return;
+
+            var list = ItemOrList<Func<object, ICommand>, List<Func<object, ICommand>>>.FromRawValue(rawValue);
+            for (var index = 0; index < MugenExtensions.Count(list); index++)
+            {
+                try
+                {
+                    (MugenExtensions.Get(list, index).Invoke(viewModel) as IDisposable)?.Dispose();
+                }
+                catch (Exception)
+                {
+                    //To avoid method access exception.
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/ViewModels/Components/ViewModelCleanerTest.cs
@@ -0,0 +1,170 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using MugenMvvm.Busy;
+using MugenMvvm.Busy.Components;
+using MugenMvvm.Commands;
+using MugenMvvm.Enums;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Busy;
+using MugenMvvm.Interfaces.Internal;
+using MugenMvvm.Interfaces.Messaging;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.ViewModels;
+using MugenMvvm.Internal;
+using MugenMvvm.Messaging;
+using MugenMvvm.Messaging.Components;
+using MugenMvvm.Metadata;
+using MugenMvvm.UnitTest.Messaging.Internal;
+using MugenMvvm.UnitTest.ViewModels.Internal;
+using MugenMvvm.UnitTest.Views.Internal;
+using MugenMvvm.ViewModels;
+using MugenMvvm.ViewModels.Components;
+using MugenMvvm.Views;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.ViewModels.Components
+{
+    public class ViewModelCleanerTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ShouldClearBusyManager()
+        {
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(new ViewManager()));
+            var viewModel = new TestCleanerViewModel
+            {
+                BusyManager = new BusyManager()
+            };
+            viewModel.BusyManager.AddComponent(new BusyManagerComponent());
+            var busyToken = viewModel.BusyManager.BeginBusy(this);
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this);
+            busyToken.IsCompleted.ShouldBeTrue();
+            viewModel.BusyManager.Components.Count.ShouldEqual(0);
+        }
+
+        [Fact]
+        public void ShouldClearMessenger()
+        {
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(new ViewManager()));
+            var viewModel = new TestCleanerViewModel
+            {
+                Messenger = new Messenger()
+            };
+            viewModel.Messenger.AddComponent(new MessengerHandlerSubscriber());
+            viewModel.Messenger.Subscribe(new TestMessengerHandlerRaw()).ShouldBeTrue();
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this);
+            viewModel.Messenger.GetSubscribers().AsList().ShouldBeEmpty();
+            viewModel.Messenger.Components.Count.ShouldEqual(0);
+        }
+
+        [Fact]
+        public void ShouldClearAttachedValues()
+        {
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(new ViewManager()));
+            var viewModel = new TestCleanerViewModel();
+            const string attachedPath = "t";
+            MugenService.AttachedValueProvider.Set(viewModel, attachedPath, this);
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this);
+            MugenService.AttachedValueProvider.Contains(viewModel, attachedPath).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ShouldClearMetadata()
+        {
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(new ViewManager()));
+            var viewModel = new TestCleanerViewModel();
+            viewModel.Metadata.Set(ViewModelMetadata.NoState, true);
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this);
+            viewModel.Metadata.TryGet(ViewModelMetadata.NoState, out _).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ShouldClearWeakReference()
+        {
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(new ViewManager()));
+            var viewModel = new TestCleanerViewModel();
+            viewModel.Value = new WeakReferenceImpl(viewModel, false);
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this);
+            viewModel.Value.Target.ShouldBeNull();
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ShouldDisposeViewModels(bool dispose)
+        {
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(new ViewManager()) { CleanupCommands = dispose });
+            var viewModel = new TestCleanerViewModel { Command1 = new CompositeCommand(), Command2 = new CompositeCommand() };
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this);
+            ((CompositeCommand)viewModel.Command1).IsDisposed.ShouldEqual(dispose);
+            ((CompositeCommand)viewModel.Command2).IsDisposed.ShouldEqual(dispose);
+        }
+
+        [Fact]
+        public void ShouldClearViews()
+        {
+            var viewModel = new TestCleanerViewModel();
+            var view = new View(new ViewModelViewMapping("1", typeof(string), typeof(IViewModelBase)), this, viewModel);
+            var viewManager = new ViewManager();
+            viewManager.AddComponent(new TestViewProviderComponent
+            {
+                TryGetViews = (o, type, arg3) =>
+                {
+                    o.ShouldEqual(viewModel);
+                    arg3.ShouldEqual(DefaultMetadata);
+                    return view;
+                }
+            });
+            int cleanupCount = 0;
+            viewManager.AddComponent(new TestViewManagerComponent
+            {
+                TryCleanupAsync = (v, o, arg3, arg4, arg5) =>
+                {
+                    ++cleanupCount;
+                    v.ShouldEqual(view);
+                    arg4.ShouldEqual(DefaultMetadata);
+                    return Task.CompletedTask;
+                }
+            });
+            var manager = new ViewModelManager();
+            manager.AddComponent(new ViewModelCleaner(viewManager));
+            manager.OnLifecycleChanged(viewModel, ViewModelLifecycleState.Disposed, this, DefaultMetadata);
+            cleanupCount.ShouldEqual(1);
+        }
+
+        #endregion
+
+        #region Nested types
+
+        private sealed class TestCleanerViewModel : TestViewModel, IHasOptionalService<IBusyManager>, IHasOptionalService<IMessenger>, IValueHolder<IWeakReference>
+        {
+            #region Properties
+
+            public ICommand? Command1 { get; set; }
+
+            public ICommand? Command2 { get; set; }
+
+            public IBusyManager? BusyManager { get; set; }
+
+            public IMessenger? Messenger { get; set; }
+
+            IBusyManager? IHasOptionalService<IBusyManager>.Service => BusyManager;
+
+            IMessenger? IHasOptionalService<IMessenger>.Service => Messenger;
+
+            public IWeakReference? Value { get; set; }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
