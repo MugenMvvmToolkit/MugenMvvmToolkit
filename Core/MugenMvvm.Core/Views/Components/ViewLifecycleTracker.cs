6573cd8a2e283106142155ab2ab36a5cc594cbdf
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-14 14:04:04 +0300

    added ViewLifecycleTracker minor changes

---
+++ Core/MugenMvvm.Core/Views/Components/ViewLifecycleTracker.cs
@@ -0,0 +1,28 @@
+﻿using MugenMvvm.Constants;
+using MugenMvvm.Enums;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.Views;
+using MugenMvvm.Interfaces.Views.Components;
+using MugenMvvm.Metadata;
+
+namespace MugenMvvm.Views.Components
+{
+    public sealed class ViewLifecycleTracker : IViewLifecycleDispatcherComponent, IHasPriority
+    {
+        #region Properties
+
+        public int Priority { get; set; } = ComponentPriority.PostInitializer;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public void OnLifecycleChanged<TState>(object view, ViewLifecycleState lifecycleState, in TState state, IReadOnlyMetadataContext? metadata)
+        {
+            (view as IView)?.Metadata.Set(ViewMetadata.LifecycleState, lifecycleState);
+        }
+
+        #endregion
+    }
+}
