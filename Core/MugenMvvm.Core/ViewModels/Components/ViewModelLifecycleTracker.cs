0bf1b4cbd6a247034d1656e0b83696a52af106a7
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-14 12:41:05 +0300

    added ViewModelLifecycleTracker minor changes

---
+++ Core/MugenMvvm.Core/ViewModels/Components/ViewModelLifecycleTracker.cs
@@ -0,0 +1,28 @@
+﻿using MugenMvvm.Constants;
+using MugenMvvm.Enums;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.ViewModels;
+using MugenMvvm.Interfaces.ViewModels.Components;
+using MugenMvvm.Metadata;
+
+namespace MugenMvvm.ViewModels.Components
+{
+    public sealed class ViewModelLifecycleTracker : IViewModelLifecycleDispatcherComponent, IHasPriority
+    {
+        #region Properties
+
+        public int Priority { get; set; } = ComponentPriority.PostInitializer;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public void OnLifecycleChanged<TState>(IViewModelBase viewModel, ViewModelLifecycleState lifecycleState, in TState state, IReadOnlyMetadataContext? metadata)
+        {
+            viewModel.Metadata.Set(ViewModelMetadata.LifecycleState, lifecycleState);
+        }
+
+        #endregion
+    }
+}
