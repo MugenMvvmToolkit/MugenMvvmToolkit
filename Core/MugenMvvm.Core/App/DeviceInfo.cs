5b5a74a30c5cee247aa3b33a86d5acba47639034
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-14 19:38:35 +0300

    added DeviceInfo minor changes

---
+++ Core/MugenMvvm.Core/App/DeviceInfo.cs
@@ -0,0 +1,39 @@
+﻿using MugenMvvm.Enums;
+using MugenMvvm.Interfaces.App;
+using MugenMvvm.Interfaces.Metadata;
+
+namespace MugenMvvm.App
+{
+    public sealed class DeviceInfo : IDeviceInfo
+    {
+        #region Constructors
+
+        public DeviceInfo(PlatformType platform, PlatformIdiom idiom, string rawVersion, IMetadataContext metadata)
+        {
+            Should.NotBeNull(platform, nameof(platform));
+            Should.NotBeNull(idiom, nameof(idiom));
+            Should.NotBeNull(rawVersion, nameof(rawVersion));
+            Should.NotBeNull(metadata, nameof(metadata));
+            Platform = platform;
+            Idiom = idiom;
+            RawVersion = rawVersion;
+            Metadata = metadata;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public PlatformType Platform { get; }
+
+        public PlatformIdiom Idiom { get; }
+
+        public string RawVersion { get; }
+
+        public bool HasMetadata => Metadata.Count != 0;
+
+        public IMetadataContext Metadata { get; }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/App/DeviceInfoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MugenMvvm.App;
+using MugenMvvm.Enums;
+using MugenMvvm.Metadata;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.App
+{
+    public class DeviceInfoTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            PlatformType type = PlatformType.Android;
+            PlatformIdiom idiom = PlatformIdiom.Desktop;
+            string version = "v";
+            var meta = new MetadataContext();
+            var deviceInfo = new DeviceInfo(type, idiom, version, meta);
+            deviceInfo.Metadata.ShouldEqual(meta);
+            deviceInfo.Idiom.ShouldEqual(idiom);
+            deviceInfo.Platform.ShouldEqual(type);
+            deviceInfo.RawVersion.ShouldEqual(version);
+            deviceInfo.HasMetadata.ShouldBeFalse();
+            meta.Set(ViewModelMetadata.Id, Guid.Empty);
+            deviceInfo.HasMetadata.ShouldBeTrue();
+        }
+
+        #endregion
+    }
+}
