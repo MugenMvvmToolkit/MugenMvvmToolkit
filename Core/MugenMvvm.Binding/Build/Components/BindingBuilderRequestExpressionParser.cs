b44c468ca1a05e212d013913b7ea11766b795828
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-18 17:22:07 +0300

    added BindingBuilderRequestExpressionParser minor changes

---
+++ Core/MugenMvvm.Binding/Build/Components/BindingBuilderRequestExpressionParser.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using MugenMvvm.Binding.Extensions.Components;
+using MugenMvvm.Binding.Interfaces.Core;
+using MugenMvvm.Binding.Interfaces.Core.Components;
+using MugenMvvm.Collections.Internal;
+using MugenMvvm.Components;
+using MugenMvvm.Constants;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.Binding.Build.Components
+{
+    public sealed class BindingBuilderRequestExpressionParser : ComponentCacheBase<IBindingManager, IBindingExpressionParserComponent>, IBindingExpressionParserComponent, IHasPriority
+    {
+        #region Fields
+
+        private readonly ReferenceLightDictionary<object?> _cache;
+
+        #endregion
+
+        #region Constructors
+
+        public BindingBuilderRequestExpressionParser()
+        {
+            _cache = new ReferenceLightDictionary<object?>(47);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Priority { get; set; } = ComponentPriority.Cache;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public ItemOrList<IBindingBuilder, IReadOnlyList<IBindingBuilder>> TryParseBindingExpression<TExpression>([DisallowNull]in TExpression expression, IReadOnlyMetadataContext? metadata)
+        {
+            if (typeof(TExpression) == typeof(BindingBuilderRequest))
+            {
+                var request = MugenExtensions.CastGeneric<TExpression, BindingBuilderRequest>(expression);
+                if (!_cache.TryGetValue(request.OriginalDelegate, out var value))
+                {
+                    value = Components.TryParseBindingExpression(request.ToConverterRequest(), metadata).GetRawValue();
+                    _cache[request.OriginalDelegate] = value;
+                }
+
+                return ItemOrList<IBindingBuilder, IReadOnlyList<IBindingBuilder>>.FromRawValue(value);
+            }
+
+            return Components.TryParseBindingExpression(expression, metadata);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void Invalidate<TState>(in TState state, IReadOnlyMetadataContext? metadata)
+        {
+            _cache.Clear();
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Binding/Build/Components/BindingBuilderRequestExpressionParserTest.cs
@@ -0,0 +1,67 @@
+﻿using MugenMvvm.Binding.Build;
+using MugenMvvm.Binding.Build.Components;
+using MugenMvvm.Binding.Core;
+using MugenMvvm.Binding.Extensions;
+using MugenMvvm.Binding.Parsing;
+using MugenMvvm.Extensions;
+using MugenMvvm.UnitTest.Binding.Core.Internal;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Build.Components
+{
+    public class BindingBuilderRequestExpressionParserTest : UnitTestBase
+    {
+        #region Fields
+
+        private static readonly ExpressionConverterRequest ConverterRequest = new ExpressionConverterRequest("", "", default);
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void TryParseBindingExpressionShouldCacheRequest()
+        {
+            var invokeCount = 0;
+            var request = BindingBuilderRequest.Get<object, object>(target => ConverterRequest);
+            var testExp = new TestBindingBuilder();
+
+            var bindingManager = new BindingManager();
+            var cache = new BindingBuilderRequestExpressionParser();
+            var component = new TestBindingExpressionParserComponent
+            {
+                TryParseBindingExpression = (o, type, arg3) =>
+                {
+                    ++invokeCount;
+                    o.ShouldEqual(ConverterRequest);
+                    type.ShouldEqual(ConverterRequest.GetType());
+                    arg3.ShouldEqual(DefaultMetadata);
+                    return testExp;
+                }
+            };
+            bindingManager.AddComponent(cache);
+            bindingManager.AddComponent(component);
+
+            bindingManager.ParseBindingExpression(request, DefaultMetadata).Item.ShouldEqual(testExp);
+            invokeCount.ShouldEqual(1);
+            bindingManager.ParseBindingExpression(request, DefaultMetadata).Item.ShouldEqual(testExp);
+            invokeCount.ShouldEqual(1);
+
+            //invalidate
+            cache.Invalidate(this, DefaultMetadata);
+            bindingManager.ParseBindingExpression(request, DefaultMetadata).Item.ShouldEqual(testExp);
+            bindingManager.ParseBindingExpression(request, DefaultMetadata).Item.ShouldEqual(testExp);
+            invokeCount.ShouldEqual(2);
+
+            //add new component
+            bindingManager.RemoveComponent(component);
+            bindingManager.AddComponent(component);
+            bindingManager.ParseBindingExpression(request, DefaultMetadata).Item.ShouldEqual(testExp);
+            bindingManager.ParseBindingExpression(request, DefaultMetadata).Item.ShouldEqual(testExp);
+            invokeCount.ShouldEqual(3);
+        }
+
+        #endregion
+    }
+}
