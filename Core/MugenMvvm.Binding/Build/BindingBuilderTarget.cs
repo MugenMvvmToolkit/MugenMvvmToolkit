ff2d1bee95a95c08afa5046d67a07da6b0b6f183
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-18 17:37:42 +0300

    added BindingBuilderTarget added IBindingBuilderContext minor changes

---
+++ Core/MugenMvvm.Binding/Build/BindingBuilderTarget.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using MugenMvvm.Binding.Interfaces.Build;
+using MugenMvvm.Binding.Interfaces.Parsing.Expressions;
+using MugenMvvm.Binding.Parsing.Expressions;
+
+namespace MugenMvvm.Binding.Build
+{
+    [StructLayout(LayoutKind.Auto)]
+    public readonly ref struct BindingBuilderTarget<TTarget, TSource>
+        where TTarget : class
+        where TSource : class
+    {
+        #region Methods
+
+        public BindingBuilderFrom<TTarget, TSource> For(string path)
+        {
+            return new BindingBuilderFrom<TTarget, TSource>(path);
+        }
+
+        public BindingBuilderFrom<TTarget, TSource> For(IExpressionNode expression)
+        {
+            return new BindingBuilderFrom<TTarget, TSource>(expression);
+        }
+
+        public BindingBuilderFrom<TTarget, TSource> For<TValue>(Expression<Func<TTarget, TValue>> expression)
+        {
+            return new BindingBuilderFrom<TTarget, TSource>(expression);
+        }
+
+        public BindingBuilderTo<TTarget, TSource> Action(Expression<Func<IBindingBuilderContext<TTarget, TSource>, object>> expression)
+        {
+            return Action<TSource>(expression);
+        }
+
+        public BindingBuilderTo<TTarget, T> Action<T>(Expression<Func<IBindingBuilderContext<TTarget, T>, object>> expression)
+            where T : class
+        {
+            return new BindingBuilderTo<TTarget, T>(new BindingBuilderFrom<TTarget, T>(MemberExpressionNode.Action), expression, default);
+        }
+
+        public BindingBuilderTo<TTarget, TSource> Action(Expression<Action<IBindingBuilderContext<TTarget, TSource>>> expression)
+        {
+            return Action<TSource>(expression);
+        }
+
+        public BindingBuilderTo<TTarget, T> Action<T>(Expression<Action<IBindingBuilderContext<TTarget, T>>> expression)
+            where T : class
+        {
+            return new BindingBuilderTo<TTarget, T>(new BindingBuilderFrom<TTarget, T>(MemberExpressionNode.Action), expression, default);
+        }
+
+        #endregion
+    }
+}
---
+++ Core/MugenMvvm.Binding/Interfaces/Build/IBindingBuilderContext.cs
@@ -0,0 +1,27 @@
+﻿using MugenMvvm.Binding.Attributes;
+using MugenMvvm.Binding.Constants;
+using MugenMvvm.Binding.Interfaces.Core;
+
+namespace MugenMvvm.Binding.Interfaces.Build
+{
+    public interface IBindingBuilderContext<out TTarget>
+        where TTarget : class
+    {
+        [BindingMacros(MacrosConstant.Binding)]
+        IBinding Binding { get; }
+
+        [BindingMacros(MacrosConstant.Target)]
+        TTarget Target { get; }
+
+        [BindingMacros(MacrosConstant.EventArgs)]
+        T EventArgs<T>();
+    }
+
+    public interface IBindingBuilderContext<out TTarget, out TSource> : IBindingBuilderContext<TTarget>
+        where TTarget : class
+        where TSource : class
+    {
+        [BindingMacros(MacrosConstant.Source)]
+        TSource Source { get; }
+    }
+}
