bf0b335556f5862518ab316ca1fa98684aff9473
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-18 14:19:04 +0300

    added BuilderListBindingExpressionBuilder minor changes

---
+++ Core/MugenMvvm.Binding/Core/Components/BuilderListBindingExpressionBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using MugenMvvm.Binding.Interfaces.Core;
+using MugenMvvm.Binding.Interfaces.Core.Components;
+using MugenMvvm.Constants;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.Binding.Core.Components
+{
+    public sealed class BuilderListBindingExpressionBuilder : IBindingExpressionBuilderComponent, IHasPriority
+    {
+        #region Properties
+
+        public int Priority { get; set; } = ComponentPriority.PostInitializer;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public ItemOrList<IBindingExpression, IReadOnlyList<IBindingExpression>> TryBuildBindingExpression<TExpression>(in TExpression expression, IReadOnlyMetadataContext? metadata)
+        {
+            if (Default.IsValueType<TExpression>() || !(expression is IReadOnlyList<IBindingExpression> result))
+                return default;
+            return ItemOrList<IBindingExpression, IReadOnlyList<IBindingExpression>>.FromRawValue(result);
+        }
+
+        #endregion
+    }
+}
