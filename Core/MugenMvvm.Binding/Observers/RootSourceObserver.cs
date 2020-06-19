ea071bc854db8795c89aaedb0defc9195a08ed7c
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-19 12:58:14 +0300

    added RootSourceObserver minor changes

---
+++ Core/MugenMvvm.Binding/Observers/RootSourceObserver.cs
@@ -0,0 +1,124 @@
+﻿using MugenMvvm.Binding.Constants;
+using MugenMvvm.Binding.Enums;
+using MugenMvvm.Binding.Extensions;
+using MugenMvvm.Binding.Interfaces.Members;
+using MugenMvvm.Binding.Interfaces.Observers;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Internal;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Internal;
+
+namespace MugenMvvm.Binding.Observers
+{
+    public sealed class RootSourceObserver : EventListenerCollection, IWeakEventListener
+    {
+        #region Fields
+
+        private readonly IWeakReference _targetRef;
+        private IWeakReference? _parentRef;
+        private ActionToken _parentToken;
+
+        #endregion
+
+        #region Constructors
+
+        private RootSourceObserver(object target, IReadOnlyMetadataContext? metadata)
+        {
+            Should.NotBeNull(target, nameof(target));
+            _targetRef = target.ToWeakReference();
+            UpdateParent(target, metadata);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsAlive => _targetRef.IsAlive;
+
+        public bool IsWeak => true;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public bool TryHandle<T>(object? sender, in T message, IReadOnlyMetadataContext? metadata)
+        {
+            var target = _targetRef.Target;
+            if (target == null)
+                return false;
+            if (TypeChecker.IsValueType<T>() || !(message is RootSourceObserver))
+            {
+                if (UpdateParent(target, metadata))
+                    Raise(target, this, metadata);
+            }
+            else
+                Raise(target, message, metadata);
+
+            return true;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Dispose()
+        {
+            var parent = _parentRef?.Target;
+            if (parent != null)
+                GetOrAdd(parent, null).Remove(this);
+            _parentRef = Default.WeakReference;
+            _parentToken.Dispose();
+            Clear();
+        }
+
+        public static RootSourceObserver GetOrAdd(object target, IReadOnlyMetadataContext? metadata)
+        {
+            return MugenService.AttachedValueProvider.GetOrAdd(target, BindingInternalConstant.RootObserver, metadata, (o, m) => new RootSourceObserver(o, m));
+        }
+
+        public static void Clear(object target)
+        {
+            if (MugenService.AttachedValueProvider.Clear(target, BindingInternalConstant.RootObserver, out var value))
+                (value as RootSourceObserver)?.Dispose();
+        }
+
+        public object? GetRoot(IReadOnlyMetadataContext? metadata)
+        {
+            return GetRoot(_targetRef.Target, metadata);
+        }
+
+        public object? GetRoot(object? target, IReadOnlyMetadataContext? metadata)
+        {
+            var parent = _parentRef?.Target;
+            if (parent == null)
+                return target;
+            return GetOrAdd(parent, metadata).GetRoot(parent, metadata);
+        }
+
+        private bool UpdateParent(object target, IReadOnlyMetadataContext? metadata)
+        {
+            var member = MugenBindingService
+                .MemberManager
+                .TryGetMember(target.GetType(), MemberType.Accessor, MemberFlags.InstanceAll, BindableMembers.Object.Parent.Name, metadata) as IAccessorMemberInfo;
+            var oldParent = _parentRef?.Target;
+            var parent = member?.GetValue(target, metadata);
+            if (oldParent == parent)
+            {
+                if (member != null && _parentRef == null && _parentToken.IsEmpty)
+                    _parentToken = member.TryObserve(target, this, metadata);
+                return false;
+            }
+
+            if (member != null && _parentToken.IsEmpty)
+                _parentToken = member.TryObserve(target, this, metadata);
+            if (oldParent != null)
+                GetOrAdd(oldParent, metadata).Remove(this);
+            if (parent != null)
+                GetOrAdd(parent, metadata).Add(this);
+            _parentRef = parent?.ToWeakReference();
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Binding/Observers/RootSourceObserverTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using MugenMvvm.Binding;
+using MugenMvvm.Binding.Interfaces.Observers;
+using MugenMvvm.Binding.Observers;
+using MugenMvvm.Internal;
+using MugenMvvm.UnitTest.Binding.Members.Internal;
+using MugenMvvm.UnitTest.Binding.Observers.Internal;
+using MugenMvvm.UnitTest.Internal.Internal;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Observers
+{
+    public class RootSourceObserverTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void ShouldReturnSelfNoParent()
+        {
+            var target = new object();
+            var observer = RootSourceObserver.GetOrAdd(target, DefaultMetadata);
+            observer.GetRoot(DefaultMetadata).ShouldEqual(target);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(target);
+        }
+
+        [Fact]
+        public void ShouldListenParent()
+        {
+            var parent = new object();
+            var target = new object();
+            IEventListener? parentListener = null;
+            var canReturnParent = false;
+            var parentMember = new TestAccessorMemberInfo
+            {
+                GetValue = (o, context) =>
+                {
+                    if (!canReturnParent)
+                        return null;
+                    if (o == target)
+                        return parent;
+                    return null;
+                },
+                TryObserve = (o, listener, arg3) =>
+                {
+                    if (o == target)
+                    {
+                        if (parentListener != null)
+                            throw new NotSupportedException();
+                        parentListener = listener;
+                        return new ActionToken((o1, o2) => parentListener = null);
+                    }
+
+                    return default;
+                }
+            };
+            using var m = TestComponentSubscriber.Subscribe(new TestMemberManagerComponent
+            {
+                TryGetMembers = (type, memberType, arg3, arg4, arg5, arg6) =>
+                {
+                    if (BindableMembers.Object.Parent.Name.Equals(arg4))
+                        return parentMember;
+                    return default;
+                }
+            });
+
+            var changedCount = 0;
+            var observer = RootSourceObserver.GetOrAdd(target, DefaultMetadata);
+            observer.Add(new TestWeakEventListener
+            {
+                TryHandle = (o, o1, arg3) =>
+                {
+                    ++changedCount;
+                    return true;
+                }
+            });
+            observer.GetRoot(DefaultMetadata).ShouldEqual(target);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(target);
+            changedCount.ShouldEqual(0);
+
+            canReturnParent = true;
+            parentListener?.TryHandle(parent, this, DefaultMetadata);
+            observer.GetRoot(DefaultMetadata).ShouldEqual(parent);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(parent);
+            changedCount.ShouldEqual(1);
+
+            canReturnParent = false;
+            var parentObserver = RootSourceObserver.GetOrAdd(parent, DefaultMetadata);
+            parentObserver.Raise(parent, parentObserver, DefaultMetadata);
+            observer.GetRoot(DefaultMetadata).ShouldEqual(parent);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(parent);
+            changedCount.ShouldEqual(2);
+
+            parentListener?.TryHandle(parent, this, DefaultMetadata);
+            observer.GetRoot(DefaultMetadata).ShouldEqual(target);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(target);
+            changedCount.ShouldEqual(3);
+
+            parentObserver.Raise(parent, parentObserver, DefaultMetadata);
+            changedCount.ShouldEqual(3);
+
+            canReturnParent = true;
+            parentListener?.TryHandle(parent, this, DefaultMetadata);
+            observer.GetRoot(DefaultMetadata).ShouldEqual(parent);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(parent);
+            changedCount.ShouldEqual(4);
+
+            RootSourceObserver.Clear(target);
+            parentObserver.Raise(parent, this, DefaultMetadata);
+            changedCount.ShouldEqual(4);
+            observer.GetRoot(DefaultMetadata).ShouldEqual(target);
+            observer.GetRoot(target, DefaultMetadata).ShouldEqual(target);
+        }
+
+        #endregion
+    }
+}
