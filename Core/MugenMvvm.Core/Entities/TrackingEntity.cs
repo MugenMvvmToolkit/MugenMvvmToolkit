c21a9d57badad514d6e6f2f19a6f9b81c26231db
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-22 17:42:30 +0300

    added TrackingEntity minor changes

---
+++ Core/MugenMvvm.Core/Entities/TrackingEntity.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using MugenMvvm.Enums;
+
+namespace MugenMvvm.Entities
+{
+    [StructLayout(LayoutKind.Auto)]
+    public readonly struct TrackingEntity
+    {
+        #region Fields
+
+        public readonly object Entity;
+        public readonly EntityState State;
+
+        #endregion
+
+        #region Constructors
+
+        public TrackingEntity(object entity, EntityState state)
+        {
+            Should.NotBeNull(entity, nameof(entity));
+            Should.NotBeNull(state, nameof(state));
+            Entity = entity;
+            State = state;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsEmpty => Entity == null;
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Entities/TrackingEntityTest.cs
@@ -0,0 +1,32 @@
+﻿using MugenMvvm.Entities;
+using MugenMvvm.Enums;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Entities
+{
+    public class TrackingEntityTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void IsEmptyShouldBeTrueDefault()
+        {
+            TrackingEntity v = default;
+            v.IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            var entity = new object();
+            var state = EntityState.Added;
+            var value = new TrackingEntity(entity, state);
+            value.IsEmpty.ShouldBeFalse();
+            value.Entity.ShouldEqual(entity);
+            value.State.ShouldEqual(state);
+        }
+
+        #endregion
+    }
+}
