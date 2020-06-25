0ce3babe8631174c5dbb49cf44befca772ec7a15
Vyacheslav Volkov <vvs0205@outlook.com>
2020-06-25 12:54:45 +0300

    added MemberTypesRequest minor changes

---
+++ Core/MugenMvvm.Binding/Members/MemberTypesRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MugenMvvm.Binding.Members
+{
+    [StructLayout(LayoutKind.Auto)]
+    public readonly struct MemberTypesRequest
+    {
+        #region Fields
+
+        public readonly string Name;
+        public readonly Type[] Types;
+
+        #endregion
+
+        #region Constructors
+
+        public MemberTypesRequest(string name, Type[] types)
+        {
+            Should.NotBeNull(name, nameof(name));
+            Should.NotBeNull(types, nameof(types));
+            Name = name;
+            Types = types;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsEmpty => Name == null;
+
+        #endregion
+    }
+}
---
+++ Tests/MugenMvvm.UnitTest/Binding/Members/MemberTypesRequestTest.cs
@@ -0,0 +1,30 @@
+﻿using MugenMvvm.Binding.Members;
+using Should;
+using Xunit;
+
+namespace MugenMvvm.UnitTest.Binding.Members
+{
+    public class MemberTypesRequestTest : UnitTestBase
+    {
+        #region Methods
+
+        [Fact]
+        public void DefaultShouldBeEmpty()
+        {
+            default(MemberTypesRequest).IsEmpty.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ConstructorShouldInitializeValues()
+        {
+            string name = "T";
+            var types = new[] {typeof(string)};
+            var memberTypesRequest = new MemberTypesRequest(name, types);
+            memberTypesRequest.IsEmpty.ShouldBeFalse();
+            memberTypesRequest.Types.ShouldEqual(types);
+            memberTypesRequest.Name.ShouldEqual(name);
+        }
+
+        #endregion
+    }
+}
