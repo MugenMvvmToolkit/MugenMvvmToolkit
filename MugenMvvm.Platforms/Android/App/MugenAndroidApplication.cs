92382615ea67580f9a5d1e648e257676f310c0f5
Vyacheslav Volkov <vvs0205@outlook.com>
2020-08-06 19:57:06 +0300

    added MugenAndroidApplication minor changes

---
+++ MugenMvvm.Platforms/Android/App/MugenAndroidApplication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using MugenMvvm.Enums;
+
+namespace MugenMvvm.Android.App
+{
+    public abstract class MugenAndroidApplication : Application
+    {
+        #region Constructors
+
+        protected MugenAndroidApplication(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
+
+        protected MugenAndroidApplication()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void OnTrimMemory(TrimMemory level)
+        {
+            if (level == TrimMemory.UiHidden)
+            {
+                MugenService.Application.OnLifecycleChanged(ApplicationLifecycleState.Deactivating, null);
+                MugenService.Application.OnLifecycleChanged(ApplicationLifecycleState.Deactivated, null);
+            }
+
+            base.OnTrimMemory(level);
+        }
+
+        #endregion
+    }
+}
