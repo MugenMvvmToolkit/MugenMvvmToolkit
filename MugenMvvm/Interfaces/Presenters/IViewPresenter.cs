0224fc5dffcec99a5a4d8ce4f82c7ec1d55e509b
Vyacheslav Volkov <vvs0205@outlook.com>
2020-08-11 11:31:35 +0300

    added IViewPresenterProviderComponent added IViewPresenter

---
+++ MugenMvvm/Interfaces/Presenters/IViewPresenter.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MugenMvvm.Enums;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Navigation;
+
+namespace MugenMvvm.Interfaces.Presenters
+{
+    public interface IViewPresenter
+    {
+        NavigationType NavigationType { get; }
+
+        Task WaitBeforeShowAsync(IViewModelPresenterMediator mediator, object? view, CancellationToken cancellationToken, IReadOnlyMetadataContext? metadata);
+
+        Task WaitBeforeCloseAsync(IViewModelPresenterMediator mediator, CancellationToken cancellationToken, IReadOnlyMetadataContext? metadata);
+
+        object? TryGetViewRequest(IViewModelPresenterMediator mediator, object? view, INavigationContext navigationContext);
+
+        bool Activate(IViewModelPresenterMediator mediator, object view, INavigationContext context);
+
+        void Show(IViewModelPresenterMediator mediator, object view, INavigationContext context);
+
+        void Close(IViewModelPresenterMediator mediator, object view, INavigationContext context);
+    }
+}
