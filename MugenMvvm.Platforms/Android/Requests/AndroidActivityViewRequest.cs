85ae92725728e308abf1f71a99caac492ea07a24
Vyacheslav Volkov <vvs0205@outlook.com>
2020-08-12 09:59:59 +0300

    added AndroidActivityViewRequest added AndroidActivityViewRequestManager minor changes

---
+++ MugenMvvm.Platforms/Android/Requests/AndroidActivityViewRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MugenMvvm.Interfaces.ViewModels;
+using MugenMvvm.Interfaces.Views;
+using MugenMvvm.Requests;
+
+namespace MugenMvvm.Android.Requests
+{
+    public class AndroidActivityViewRequest : ViewModelViewRequest
+    {
+        #region Constructors
+
+        public AndroidActivityViewRequest(IViewModelBase viewModel, IViewMapping mapping, Action startActivity)
+            : base(viewModel, null)
+        {
+            Should.NotBeNull(viewModel, nameof(viewModel));
+            Should.NotBeNull(mapping, nameof(mapping));
+            Should.NotBeNull(startActivity, nameof(startActivity));
+            StartActivity = startActivity;
+            Mapping = mapping;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Action StartActivity { get; }
+
+        public IViewMapping Mapping { get; }
+
+        #endregion
+    }
+}
---
+++ MugenMvvm.Platforms/Android/Views/AndroidActivityViewRequestManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MugenMvvm.Android.Enums;
+using MugenMvvm.Android.Requests;
+using MugenMvvm.Components;
+using MugenMvvm.Constants;
+using MugenMvvm.Enums;
+using MugenMvvm.Extensions;
+using MugenMvvm.Extensions.Components;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+using MugenMvvm.Interfaces.Views;
+using MugenMvvm.Interfaces.Views.Components;
+
+namespace MugenMvvm.Android.Views
+{
+    public sealed class AndroidActivityViewRequestManager : ComponentDecoratorBase<IViewManager, IViewManagerComponent>, IViewManagerComponent, IHasPriority
+    {
+        #region Properties
+
+        public int Priority { get; set; } = ViewComponentPriority.ViewModelViewProviderDecorator + 1;
+
+        #endregion
+
+        #region Implementation of interfaces
+
+        public Task<IView>? TryInitializeAsync(IViewManager viewManager, IViewMapping mapping, object request, CancellationToken cancellationToken, IReadOnlyMetadataContext? metadata)
+        {
+            if (!(request is AndroidActivityViewRequest activityRequest))
+                return Components.TryInitializeAsync(viewManager, mapping, request, cancellationToken, metadata);
+            var handler = new PendingActivityHandler(activityRequest.Mapping);
+            viewManager.AddComponent(handler);
+            activityRequest.StartActivity();
+
+            var tcs = new TaskCompletionSource<IView>();
+            handler.Task.ContinueWithEx((tcs, Components, viewManager, mapping, activityRequest, metadata), (task, tuple) =>
+            {
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    tuple.tcs.TrySetFromTask(task);
+                    return;
+                }
+
+                activityRequest.View = task.Result;
+                var result = tuple.Components.TryInitializeAsync(tuple.viewManager, tuple.mapping, tuple.activityRequest, default, tuple.metadata);
+                if (result == null)
+                    ExceptionManager.ThrowObjectNotInitialized(tuple.Components);
+                tuple.tcs.TrySetFromTask(result);
+            }).ContinueWith((task, o) => ((TaskCompletionSource<IView>) o).TrySetFromTask(task), tcs, TaskContinuationOptions.NotOnRanToCompletion);
+            return tcs.Task;
+        }
+
+        public Task? TryCleanupAsync(IViewManager viewManager, IView view, object? state, CancellationToken cancellationToken, IReadOnlyMetadataContext? metadata)
+            => Components.TryCleanupAsync(viewManager, view, state, cancellationToken, metadata);
+
+        #endregion
+
+        #region Nested types
+
+        private sealed class PendingActivityHandler : TaskCompletionSource<object>, IViewLifecycleDispatcherComponent, IHasPriority
+        {
+            #region Fields
+
+            private readonly IViewMapping _mapping;
+
+            #endregion
+
+            #region Constructors
+
+            public PendingActivityHandler(IViewMapping mapping)
+            {
+                _mapping = mapping;
+            }
+
+            #endregion
+
+            #region Properties
+
+            public int Priority => ViewComponentPriority.PreInitializer;
+
+            #endregion
+
+            #region Implementation of interfaces
+
+            public void OnLifecycleChanged(IViewManager viewManager, object view, ViewLifecycleState lifecycleState, object? state, IReadOnlyMetadataContext? metadata)
+            {
+                if (view is IView || lifecycleState != AndroidViewLifecycleState.Created || !_mapping.ViewType.IsInstanceOfType(view))
+                    return;
+                viewManager.RemoveComponent(this);
+                TrySetResult(view);
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
