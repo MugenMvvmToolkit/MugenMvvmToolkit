254643e30f56cb8bd67ccb335fc73c75a8533ffa
Vyacheslav Volkov <vvs0205@outlook.com>
2020-10-31 16:05:41 +0300

    minor changes

--- MugenMvvm/Bindings/Observation/Paths/MultiMemberPath.cs
+++ MugenMvvm/Bindings/Observation/Paths/MultiMemberPath.cs
@@ -25,41 +25,58 @@ namespace MugenMvvm.Bindings.Observation.Paths
                 return;
             }
 
-            var items = new List<string>();
+            int size = 0;
 #if SPAN_API
             var span = path.AsSpan();
-            foreach (var range in path.AsSpan().Split(MugenBindingExtensions.DotChar))
+            var enumerator = span.Split(MugenBindingExtensions.DotChar);
+            foreach (var range in enumerator)
+            {
+                var s = span[range];
+                size += s.IndexOf('[') <= 0 || s.IndexOf(']') < 0 ? 1 : 2;
+            }
+
+            var members = new string[size];
+            size = 0;
+            foreach (var range in enumerator)
             {
                 var s = span[range];
                 var start = s.IndexOf('[');
                 var end = s.IndexOf(']');
                 if (start <= 0 || end < 0)
                 {
-                    items.Add(s.Trim().ToString());
+                    members[size++] = s.Trim().ToString();
                     continue;
                 }
 
-                items.Add(s.Slice(0, start).Trim().ToString());
-                items.Add(s.Slice(start, end - start + 1).Trim().ToString());
+                members[size++] = s.Slice(0, start).Trim().ToString();
+                members[size++] = s.Slice(start, end - start + 1).Trim().ToString();
             }
 #else
-            Members = path.Split(MugenBindingExtensions.DotSeparator, StringSplitOptions.RemoveEmptyEntries);
-            for (var index = 0; index < Members.Count; index++)
+            var split = path.Split(MugenBindingExtensions.DotSeparator, StringSplitOptions.RemoveEmptyEntries);
+            for (var index = 0; index < split.Length; index++)
             {
-                var s = Members[index];
+                var s = split[index];
+                size += s.IndexOf('[') <= 0 || s.IndexOf(']') < 0 ? 1 : 2;
+            }
+
+            var members = new string[size];
+            size = 0;
+            for (var index = 0; index < split.Length; index++)
+            {
+                var s = split[index];
                 var start = s.IndexOf('[');
                 var end = s.IndexOf(']');
                 if (start <= 0 || end < 0)
                 {
-                    items.Add(s.Trim());
+                    members[size++] = s.Trim();
                     continue;
                 }
 
-                items.Add(s.Substring(0, start).Trim());
-                items.Add(s.Substring(start, end - start + 1).Trim());
+                members[size++] = s.Substring(0, start).Trim();
+                members[size++] = s.Substring(start, end - start + 1).Trim();
             }
 #endif
-            Members = items;
+            Members = members;
         }
 
         #endregion
