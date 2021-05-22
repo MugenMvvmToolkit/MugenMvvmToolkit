e8b0f3e6daa803aa01b5e7b74f8936ac3a4b688c
Vyacheslav Volkov <vvs0205@outlook.com>
2021-05-22 21:12:24 +0300

    added LimitCollectionDecorator

---
+++ MugenMvvm.UnitTests/Collections/Components/LimitCollectionDecoratorTest.cs
@@ -0,0 +1,402 @@
+﻿using System.Collections.Generic;
+using MugenMvvm.Collections;
+using MugenMvvm.Collections.Components;
+using MugenMvvm.Extensions;
+using MugenMvvm.Internal;
+using MugenMvvm.UnitTests.Collections.Internal;
+using Should;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MugenMvvm.UnitTests.Collections.Components
+{
+    public class LimitCollectionDecoratorTest : UnitTestBase
+    {
+        private readonly SynchronizedObservableCollection<object> _collection;
+        private readonly LimitCollectionDecorator<int> _decorator;
+        private readonly DecoratorObservableCollectionTracker<object> _tracker;
+
+        public LimitCollectionDecoratorTest(ITestOutputHelper? outputHelper = null) : base(outputHelper)
+        {
+            _collection = new SynchronizedObservableCollection<object>(ComponentCollectionManager);
+            _decorator = new LimitCollectionDecorator<int>(null, i => i % 2 == 0);
+            _collection.AddComponent(_decorator);
+            _tracker = new DecoratorObservableCollectionTracker<object>();
+            _collection.AddComponent(_tracker);
+            _tracker.Changed += Assert;
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void AddShouldTrackChanges(int? limit)
+        {
+            _decorator.Limit = limit;
+            for (var i = 0; i < 100; i++)
+            {
+                _collection.Add(i);
+                Assert();
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.Insert(i, i);
+                Assert();
+            }
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ChangeShouldTrackChanges1(int? limit)
+        {
+            var ignoreIds = new HashSet<int>();
+            _decorator.Limit = limit;
+            _decorator.Condition = i => !ignoreIds.Contains(i);
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < _collection.Count; i++)
+            {
+                ignoreIds.Add(i);
+                _collection.RaiseItemChanged(_collection[i], null);
+                Assert();
+
+                ignoreIds.Remove(i);
+                _collection.RaiseItemChanged(_collection[i], null);
+                Assert();
+
+                if (limit == null)
+                    _tracker.ItemChangedCount.ShouldEqual((i + 1) * 2);
+                else
+                    _tracker.ItemChangedCount.ShouldEqual(0);
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ChangeShouldTrackChanges2(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            int countLimit = 0;
+            int count = 0;
+            for (var i = 0; i < _collection.Count; i++)
+            {
+                if (IsSatisfied(i))
+                {
+                    if (countLimit < limit.GetValueOrDefault(int.MaxValue))
+                        ++countLimit;
+                }
+                else
+                    ++count;
+
+                _collection.RaiseItemChanged(_collection[i], null);
+                Assert();
+                _tracker.ItemChangedCount.ShouldEqual(count + countLimit);
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ClearShouldTrackChanges(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            _collection.Clear();
+            Assert();
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void MoveShouldTrackChanges1(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.Move(i, i + 1);
+                Assert();
+            }
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void MoveShouldTrackChanges2(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 1; i < 10; i++)
+            {
+                _collection.Move(i, i * 2 + i);
+                Assert();
+            }
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void RemoveShouldTrackChanges(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 20; i++)
+            {
+                _collection.Remove(i);
+                Assert();
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.RemoveAt(i);
+                Assert();
+            }
+
+            var count = _collection.Count;
+            for (var i = 0; i < count; i++)
+            {
+                _collection.RemoveAt(0);
+                Assert();
+            }
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ReplaceShouldTrackChanges1(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection[i] = i + 101;
+                Assert();
+            }
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ReplaceShouldTrackChanges2(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            for (var j = 10; j < 20; j++)
+            {
+                _collection[i] = _collection[j];
+                Assert();
+            }
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ResetShouldTrackChanges(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            _collection.Reset(new object[] {1, 2, 3, 4, 5});
+            Assert();
+
+            _decorator.Limit = null;
+            Assert();
+            _decorator.Limit = limit;
+            Assert();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ShouldAttachDetach(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            _collection.RemoveComponent(_decorator);
+
+            for (var i = 0; i < 2; i++)
+            {
+                _collection.AddComponent(_decorator);
+                if (i != 0)
+                    _collection.Add(i);
+
+                Assert();
+                _collection.RemoveComponent(_decorator);
+                Assert();
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(int.MaxValue)]
+        [InlineData(null)]
+        public void ShouldTrackChanges(int? limit)
+        {
+            _decorator.Limit = limit;
+
+            _collection.Add(1);
+            Assert();
+
+            _collection.Insert(1, 2);
+            Assert();
+
+            _collection.Move(0, 1);
+            Assert();
+
+            _collection.Remove(2);
+            Assert();
+
+            _collection.RemoveAt(0);
+            Assert();
+
+            _collection.Reset(new object[] {1, 2, 3, 4, 5});
+            Assert();
+
+            _collection[0] = 200;
+            Assert();
+
+            _collection.Clear();
+            Assert();
+        }
+
+        private void Assert()
+        {
+            _tracker.ChangedItems.ShouldEqual(_collection.Decorate());
+            _tracker.ChangedItems.ShouldEqual(_decorator.Limit == null ? _collection : Decorate(_decorator.Limit.Value));
+        }
+
+        private IEnumerable<object?> Decorate(int limit)
+        {
+            var count = 0;
+            foreach (var item in _collection)
+            {
+                if (IsSatisfied(item) && ++count > limit)
+                    continue;
+
+                yield return item;
+            }
+        }
+
+        private bool IsSatisfied(object? item) => item is int i && (_decorator.Condition == null || _decorator.Condition(i));
+    }
+}
---
+++ MugenMvvm/Collections/Components/LimitCollectionDecorator.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MugenMvvm.Components;
+using MugenMvvm.Constants;
+using MugenMvvm.Extensions;
+using MugenMvvm.Interfaces.Collections.Components;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace MugenMvvm.Collections.Components
+{
+    public class LimitCollectionDecorator<T> : AttachableComponentBase<ICollection>, ICollectionDecorator, IHasPriority, IComparer<LimitCollectionDecorator<T>.ItemInfo>
+    {
+        private const int NotFound = -1;
+        private readonly List<ItemInfo> _items;
+        private Func<T, bool>? _condition;
+        private int? _limit;
+
+        public LimitCollectionDecorator(int? limit = null, Func<T, bool>? condition = null, int priority = CollectionComponentPriority.LimitDecorator)
+        {
+            _items = new List<ItemInfo>();
+            _condition = condition;
+            _limit = limit;
+            Priority = priority;
+        }
+
+        public Func<T, bool>? Condition
+        {
+            get => _condition;
+            set
+            {
+                if (_condition != value)
+                    Update(value, _limit);
+            }
+        }
+
+        public int? Limit
+        {
+            get => _limit;
+            set
+            {
+                if (_limit != value)
+                    Update(_condition, value);
+            }
+        }
+
+        public int Count => _items.Count;
+
+        public int Priority { get; set; }
+
+        protected ICollectionDecoratorManagerComponent? DecoratorManager { get; private set; }
+
+        private bool HasCondition => Limit != null && DecoratorManager != null;
+
+        protected override void OnAttached(ICollection owner, IReadOnlyMetadataContext? metadata) => DecoratorManager = CollectionDecoratorManager.GetOrAdd(owner);
+
+        protected override void OnDetached(ICollection owner, IReadOnlyMetadataContext? metadata)
+        {
+            _items.Clear();
+            DecoratorManager = null;
+        }
+
+        private void Update(Func<T, bool>? condition, int? limit)
+        {
+            if (DecoratorManager == null)
+            {
+                _condition = condition;
+                _limit = limit;
+                return;
+            }
+
+            using var t = DecoratorManager.TryLock(Owner, this);
+            _condition = condition;
+            _limit = limit;
+            _items.Clear();
+
+            var items = DecoratorManager.Decorate(Owner, this);
+            if (HasCondition)
+            {
+                UpdateItems(items);
+                items = Decorate(items);
+            }
+
+            DecoratorManager.OnReset(Owner, this, items);
+        }
+
+        private bool IsSatisfied(object? item)
+        {
+            if (_condition == null)
+                return item is T;
+            return item is T t && _condition(t);
+        }
+
+        private int IndexOf(int index)
+        {
+            var i = _items.BinarySearch(new ItemInfo(default!, index), this);
+            if (i < 0)
+                return NotFound;
+            return i;
+        }
+
+        private void Replace(ICollection collection, bool oldSatisfied, bool newSatisfied, object? oldItem, object? newItem, int index)
+        {
+            using var t = DecoratorManager!.BatchUpdate(collection, this);
+            if (!oldSatisfied || Remove((T) oldItem!, index))
+            {
+                if (!oldSatisfied)
+                    UpdateIndexes(index, -1);
+                DecoratorManager!.OnRemoved(collection, this, oldItem, GetIndex(index));
+            }
+
+            if (!newSatisfied || Add((T) newItem!, index))
+            {
+                if (!newSatisfied)
+                    UpdateIndexes(index, 1);
+                DecoratorManager!.OnAdded(collection, this, newItem, GetIndex(index));
+            }
+        }
+
+        private IEnumerable<object?> Decorate(IEnumerable<object?> items)
+        {
+            var count = 0;
+            foreach (var item in items)
+            {
+                if (IsSatisfied(item) && ++count > Limit)
+                    continue;
+
+                yield return item;
+            }
+        }
+
+        private void UpdateItems(IEnumerable<object?> items)
+        {
+            var index = 0;
+            foreach (var item in items)
+            {
+                if (IsSatisfied(item))
+                    MugenExtensions.AddOrdered(_items, new ItemInfo((T) item!, index), this);
+                ++index;
+            }
+        }
+
+        private bool Add(T item, int index)
+        {
+            UpdateIndexes(index, 1);
+            var newIndex = MugenExtensions.AddOrdered(_items, new ItemInfo(item, index), this);
+            var limit = Limit!.Value;
+            if (limit == 0 || newIndex >= limit)
+                return false;
+
+            if (_items.Count <= limit)
+                return true;
+
+            var oldItem = _items[limit];
+            if (index == oldItem.OriginalIndex - 1)
+            {
+                DecoratorManager!.OnReplaced(Owner, this, oldItem.Item, item, GetIndex(index));
+                return false;
+            }
+
+            DecoratorManager!.OnAdded(Owner, this, item, GetIndex(index));
+            DecoratorManager!.OnRemoved(Owner, this, oldItem.Item, GetIndex(oldItem.OriginalIndex));
+            return false;
+        }
+
+        private bool Remove(T item, int index)
+        {
+            var indexToRemove = IndexOf(index);
+            _items.RemoveAt(indexToRemove);
+            UpdateIndexes(index, -1);
+            var limit = Limit!.Value;
+            if (limit == 0 || indexToRemove >= limit)
+                return false;
+
+            if (_items.Count + 1 <= limit)
+                return true;
+
+            var oldItem = _items[limit - 1];
+            if (index == oldItem.OriginalIndex)
+            {
+                DecoratorManager!.OnReplaced(Owner, this, item, oldItem.Item, GetIndex(index));
+                return false;
+            }
+
+            DecoratorManager!.OnRemoved(Owner, this, item, GetIndex(index));
+            DecoratorManager!.OnAdded(Owner, this, oldItem.Item, GetIndex(oldItem.OriginalIndex));
+
+            return false;
+        }
+
+        private int GetIndex(int index)
+        {
+            if (Limit!.Value > index || Limit!.Value >= _items.Count)
+                return index;
+
+            var lastIndex = _items.BinarySearch(new ItemInfo(default!, index), this);
+            if (lastIndex == 0)
+                return index;
+            if (lastIndex < 0)
+                lastIndex = ~lastIndex;
+            if (lastIndex <= Limit.Value)
+                return index;
+            return index - (lastIndex - Limit.Value);
+        }
+
+        private void UpdateIndexes(int index, int value)
+        {
+#if NET5_0
+            var items = CollectionsMarshal.AsSpan(_items);
+            for (var i = items.Length - 1; i >= 0; i--)
+#else
+            var items = _items;
+            for (var i = items.Count - 1; i >= 0; i--)
+#endif
+            {
+                var item = items[i];
+                if (item.OriginalIndex >= index)
+                {
+#if NET5_0
+                    items[i].OriginalIndex += value;
+#else
+                    items[i] = new ItemInfo(item.Item, item.OriginalIndex + value);
+#endif
+                }
+                else
+                    break;
+            }
+        }
+
+        IEnumerable<object?> ICollectionDecorator.Decorate(ICollection collection, IEnumerable<object?> items) => HasCondition ? Decorate(items) : items;
+
+        bool ICollectionDecorator.OnChanged(ICollection collection, ref object? item, ref int index, ref object? args)
+        {
+            if (!HasCondition)
+                return true;
+
+            var currentIndex = IndexOf(index);
+            var oldSatisfied = currentIndex != NotFound;
+            var newSatisfied = IsSatisfied(item);
+
+            if (oldSatisfied == newSatisfied)
+            {
+                if (oldSatisfied && currentIndex >= Limit)
+                    return false;
+
+                index = GetIndex(index);
+                return true;
+            }
+
+            Replace(collection, oldSatisfied, newSatisfied, item, item, index);
+            return false;
+        }
+
+        bool ICollectionDecorator.OnAdded(ICollection collection, ref object? item, ref int index)
+        {
+            if (!HasCondition)
+                return true;
+
+            if (IsSatisfied(item))
+            {
+                using var t = DecoratorManager!.BatchUpdate(collection, this);
+                if (!Add((T) item!, index))
+                    return false;
+            }
+            else
+                UpdateIndexes(index, 1);
+
+            index = GetIndex(index);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnReplaced(ICollection collection, ref object? oldItem, ref object? newItem, ref int index)
+        {
+            if (!HasCondition)
+                return true;
+
+            var oldSatisfied = IsSatisfied(oldItem);
+            var newSatisfied = IsSatisfied(newItem);
+
+            if (!oldSatisfied && !newSatisfied)
+            {
+                index = GetIndex(index);
+                return true;
+            }
+
+            Replace(collection, oldSatisfied, newSatisfied, oldItem, newItem, index);
+            return false;
+        }
+
+        bool ICollectionDecorator.OnMoved(ICollection collection, ref object? item, ref int oldIndex, ref int newIndex)
+        {
+            if (!HasCondition)
+                return true;
+
+            if (oldIndex < Limit && newIndex < Limit || !IsSatisfied(item))
+            {
+                var toRemove = IndexOf(oldIndex);
+                UpdateIndexes(oldIndex + 1, -1);
+                UpdateIndexes(newIndex, 1);
+                if (toRemove != NotFound)
+                {
+                    _items.RemoveAt(toRemove);
+                    MugenExtensions.AddOrdered(_items, new ItemInfo((T) item!, newIndex), this);
+                }
+
+                oldIndex = GetIndex(oldIndex);
+                newIndex = GetIndex(newIndex);
+                return true;
+            }
+
+            using var t = DecoratorManager!.BatchUpdate(collection, this);
+            var value = (T) item!;
+            Remove(value, oldIndex);
+            Add(value, newIndex);
+            return false;
+        }
+
+        bool ICollectionDecorator.OnRemoved(ICollection collection, ref object? item, ref int index)
+        {
+            if (!HasCondition)
+                return true;
+
+            if (IsSatisfied(item))
+            {
+                using var t = DecoratorManager!.BatchUpdate(collection, this);
+                if (!Remove((T) item!, index))
+                    return false;
+            }
+            else
+                UpdateIndexes(index, -1);
+
+            index = GetIndex(index);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnReset(ICollection collection, ref IEnumerable<object?>? items)
+        {
+            if (!HasCondition)
+                return true;
+
+            _items.Clear();
+            if (items != null)
+            {
+                UpdateItems(items);
+                items = Decorate(items);
+            }
+
+            return true;
+        }
+
+        int IComparer<ItemInfo>.Compare(ItemInfo x, ItemInfo y) => x.OriginalIndex.CompareTo(y.OriginalIndex);
+
+        [StructLayout(LayoutKind.Auto)]
+        private struct ItemInfo : IEquatable<ItemInfo>
+        {
+            // ReSharper disable FieldCanBeMadeReadOnly.Local
+            public T Item;
+
+            public int OriginalIndex;
+            // ReSharper restore FieldCanBeMadeReadOnly.Local
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public ItemInfo(T item, int originalIndex)
+            {
+                Item = item;
+                OriginalIndex = originalIndex;
+            }
+
+            public bool Equals(ItemInfo other) => OriginalIndex == other.OriginalIndex;
+        }
+    }
+}
