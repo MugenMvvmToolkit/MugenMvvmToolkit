34bc19fbfa36066881663f24dda674528e21c437
Vyacheslav Volkov <vvs0205@outlook.com>
2021-05-25 12:23:43 +0300

    added ItemConverterCollectionDecorator

---
+++ MugenMvvm.UnitTests/Collections/Components/ItemConverterCollectionDecoratorTest.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MugenMvvm.Collections;
+using MugenMvvm.Collections.Components;
+using MugenMvvm.Extensions;
+using MugenMvvm.UnitTests.Collections.Internal;
+using Should;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MugenMvvm.UnitTests.Collections.Components
+{
+    public class ItemConverterCollectionDecoratorTest : UnitTestBase
+    {
+        private readonly SynchronizedObservableCollection<object> _collection;
+        private readonly DecoratorObservableCollectionTracker<object> _tracker;
+        private readonly ItemConverterCollectionDecorator _decorator;
+
+        public ItemConverterCollectionDecoratorTest(ITestOutputHelper? outputHelper = null) : base(outputHelper)
+        {
+            _collection = new SynchronizedObservableCollection<object>(ComponentCollectionManager);
+            _tracker = new DecoratorObservableCollectionTracker<object>();
+            _collection.AddComponent(_tracker);
+            _decorator = new ItemConverterCollectionDecorator(o => "Item: " + o);
+            _collection.AddComponent(_decorator);
+            _tracker.Changed += Assert;
+        }
+
+        [Fact]
+        public void AddShouldTrackChanges()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                _collection.Add(i);
+                Assert();
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.Insert(i, i);
+                Assert();
+            }
+        }
+
+        [Fact]
+        public void ChangeShouldTrackChanges()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                _collection.Add(i);
+                Assert();
+            }
+
+            for (var i = 0; i < _collection.Count; i++)
+            {
+                _collection.RaiseItemChanged(_collection[i], null);
+                Assert();
+                _tracker.ItemChangedCount.ShouldEqual(i + 1);
+            }
+        }
+
+        [Fact]
+        public void ClearShouldTrackChanges()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            _collection.Clear();
+            Assert();
+        }
+
+        [Fact]
+        public void MoveShouldTrackChanges1()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.Move(i, i + 1);
+                Assert();
+            }
+        }
+
+        [Fact]
+        public void MoveShouldTrackChanges2()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.Move(i, i * 2 + 1);
+                Assert();
+            }
+        }
+
+        [Fact]
+        public void RemoveShouldTrackChanges()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 20; i++)
+            {
+                _collection.Remove(i);
+                Assert();
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection.RemoveAt(i);
+                Assert();
+            }
+
+            var count = _collection.Count;
+            for (var i = 0; i < count; i++)
+            {
+                _collection.RemoveAt(0);
+                Assert();
+            }
+        }
+
+        [Fact]
+        public void ReplaceShouldTrackChanges1()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            {
+                _collection[i] = i + 101;
+                Assert();
+            }
+        }
+
+        [Fact]
+        public void ReplaceShouldTrackChanges2()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            for (var i = 0; i < 10; i++)
+            for (var j = 10; j < 20; j++)
+            {
+                _collection[i] = _collection[j];
+                Assert();
+            }
+        }
+
+        [Fact]
+        public void ResetShouldTrackChanges()
+        {
+            for (var i = 0; i < 100; i++)
+                _collection.Add(i);
+            Assert();
+
+            _collection.Reset(new object[] {1, 2, 3, 4, 5});
+            Assert();
+        }
+
+        [Fact]
+        public void ShouldTrackChanges()
+        {
+            for (var i = 0; i < 4; i++)
+            {
+                _collection.Add(1);
+                Assert();
+
+                _collection.Insert(1, 2);
+                Assert();
+
+                _collection.Move(0, 1);
+                Assert();
+
+                _collection.Move(1, 0);
+                Assert();
+
+                _collection.Remove(2);
+                Assert();
+
+                _collection.RemoveAt(0);
+                Assert();
+
+                _collection.Reset(new object[] {1, 2, 3, 4, 5, i});
+                Assert();
+
+                _collection[0] = 200;
+                Assert();
+
+                _collection[3] = 3;
+                Assert();
+
+                _collection.Move(0, _collection.Count - 1);
+                Assert();
+
+                _collection.Move(0, _collection.Count - 2);
+                Assert();
+
+                _collection[i] = i;
+                Assert();
+            }
+
+            _collection.Clear();
+            Assert();
+        }
+
+        private void Assert()
+        {
+            _tracker.ChangedItems.ShouldEqual(_collection.Decorate());
+            _tracker.ChangedItems.ShouldEqual(Decorate().ToArray());
+        }
+
+        private IEnumerable<object?> Decorate() => _collection.Select(_decorator.Converter);
+    }
+}
---
+++ MugenMvvm/Collections/Components/ItemConverterCollectionDecorator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MugenMvvm.Constants;
+using MugenMvvm.Interfaces.Collections.Components;
+using MugenMvvm.Interfaces.Components;
+using MugenMvvm.Interfaces.Metadata;
+using MugenMvvm.Interfaces.Models;
+
+namespace MugenMvvm.Collections.Components
+{
+    public sealed class ItemConverterCollectionDecorator : IAttachableComponent, ICollectionDecorator, IHasPriority
+    {
+        public ItemConverterCollectionDecorator(Func<object?, object?> converter, int priority = CollectionComponentPriority.ConverterDecorator)
+        {
+            Should.NotBeNull(converter, nameof(converter));
+            Converter = converter;
+            Priority = priority;
+        }
+
+        public Func<object?, object?> Converter { get; }
+
+        public int Priority { get; set; }
+
+        bool IAttachableComponent.OnAttaching(object owner, IReadOnlyMetadataContext? metadata) => true;
+
+        void IAttachableComponent.OnAttached(object owner, IReadOnlyMetadataContext? metadata) => CollectionDecoratorManager.GetOrAdd((IEnumerable) owner);
+
+        IEnumerable<object?> ICollectionDecorator.Decorate(ICollection collection, IEnumerable<object?> items) => items.Select(Converter);
+
+        bool ICollectionDecorator.OnChanged(ICollection collection, ref object? item, ref int index, ref object? args)
+        {
+            item = Converter(item);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnAdded(ICollection collection, ref object? item, ref int index)
+        {
+            item = Converter(item);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnReplaced(ICollection collection, ref object? oldItem, ref object? newItem, ref int index)
+        {
+            oldItem = Converter(oldItem);
+            newItem = Converter(newItem);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnMoved(ICollection collection, ref object? item, ref int oldIndex, ref int newIndex)
+        {
+            item = Converter(item);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnRemoved(ICollection collection, ref object? item, ref int index)
+        {
+            item = Converter(item);
+            return true;
+        }
+
+        bool ICollectionDecorator.OnReset(ICollection collection, ref IEnumerable<object?>? items)
+        {
+            items = items?.Select(Converter);
+            return true;
+        }
+    }
+}
