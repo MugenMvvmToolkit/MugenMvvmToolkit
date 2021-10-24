00975f4fd4a10251ebc2efbf44c6b6db97c80523
Vyacheslav Volkov <vvs0205@outlook.com>
2021-10-24 20:13:12 +0300

    added InvocationExpressionConverter

---
+++ MugenMvvm.UnitTests/Bindings/Parsing/Components/Converters/InvocationExpressionConverterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+using MugenMvvm.Bindings.Parsing.Components.Converters;
+using MugenMvvm.Bindings.Parsing.Expressions;
+using Xunit;
+
+namespace MugenMvvm.UnitTests.Bindings.Parsing.Components.Converters
+{
+    public class InvocationExpressionConverterTest : ExpressionConverterTestBase<InvocationExpressionConverter>
+    {
+        [Fact]
+        public void TryConvertShouldConvertInvocationExpression()
+        {
+            var action = new Action<int>(i => { });
+            var expression = Expression.Invoke(Expression.Constant(action), Expression.Constant(1));
+            var expectedResult = new MethodCallExpressionNode(ConstantExpressionNode.Get(action), nameof(action.Invoke), ConstantExpressionNode.Get(1));
+            Context.SetExpression(expression.Expression, expectedResult.Target!);
+            Context.SetExpression(expression.Arguments[0], expectedResult.Arguments[0]);
+            Converter.TryConvert(Context, expression).ShouldEqual(expectedResult);
+        }
+    }
+}
---
+++ MugenMvvm/Bindings/Parsing/Components/Converters/InvocationExpressionConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+using MugenMvvm.Bindings.Constants;
+using MugenMvvm.Bindings.Extensions;
+using MugenMvvm.Bindings.Interfaces.Parsing;
+using MugenMvvm.Bindings.Interfaces.Parsing.Components;
+using MugenMvvm.Bindings.Interfaces.Parsing.Expressions;
+using MugenMvvm.Bindings.Parsing.Expressions;
+using MugenMvvm.Collections;
+using MugenMvvm.Interfaces.Models;
+
+namespace MugenMvvm.Bindings.Parsing.Components.Converters
+{
+    public sealed class InvocationExpressionConverter : IExpressionConverterComponent<Expression>, IHasPriority
+    {
+        public int Priority { get; init; } = ParsingComponentPriority.Method;
+
+        public IExpressionNode? TryConvert(IExpressionConverterContext<Expression> context, Expression expression)
+        {
+            if (expression is not InvocationExpression invocationExpression || !typeof(Delegate).IsAssignableFrom(invocationExpression.Expression.Type))
+                return null;
+
+            return new MethodCallExpressionNode(context.Convert(invocationExpression.Expression), nameof(Action.Invoke),
+                context.Convert(ItemOrIReadOnlyList.FromList(invocationExpression.Arguments)));
+        }
+    }
+}
